3e30cb5b13b9a3c358334eda6763717546b5a9d4
Sean O'Hearn <seohearn1@gmail.com>
2019-08-31 18:05:20 -0700

    General idea for folder structure, separation of logic, and shared functionalities.

---
+++ src/components/hero/hero-navbar.view.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import HeroNavButtonsView from './hero-nav-buttons.view';
+import './hero.scss';
+
+const HeroNavbarView: React.FC = () => {
+  return <HeroNavButtonsView />;
+};
+
+export default HeroNavbarView;
---
+++ src/components/hero/hero.view.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import HeroLightBulb from './hero-lightbulb.view';
+import HeroNavbarView from './hero-navbar.view';
+import HeroMlhBadgeView from './hero-mlh-badge.view';
+import HeroTitleView from './hero-title.view';
+import HeroSloganView from './hero-slogan.view';
+import './hero.scss';
+
+const HeroView: React.FC = () => {
+  return (
+    <>
+      <div className="hero-header-container">
+        <HeroLightBulb />
+        <HeroNavbarView />
+        <HeroMlhBadgeView />
+      </div>
+      <HeroTitleView />
+      <HeroSloganView />
+    </>
+  );
+};
+
+export default HeroView;
