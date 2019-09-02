d4eb096cd833ed68aa2bf06ee4e0428bdadfa42a
Sean O'Hearn <seohearn1@gmail.com>
2019-09-02 16:06:04 -0700

    committing current progress for Hero to merge

--- src/components/hero/hero.view.tsx
+++ src/components/hero/hero.view.tsx
@@ -4,6 +4,11 @@ import HeroNavbarView from './hero-navbar.view';
 import HeroMlhBadgeView from './hero-mlh-badge.view';
 import HeroTitleView from './hero-title.view';
 import HeroSloganView from './hero-slogan.view';
+import HeroRightSideView from './hero-rightside.view';
+import HeroInfoTextView from './hero-event-info-text.view';
+import HeroSponsorUsButtonView from './hero-sponsor-us-button.view';
+import HeroNavigateToBottomButton from './hero-navigate-to-bottom-button';
+import HeroBackdropView from './backdrop/hero-backdrop.view';
 import './hero.scss';
 
 const HeroView: React.FC = () => {
@@ -15,9 +20,19 @@ const HeroView: React.FC = () => {
         <HeroMlhBadgeView />
       </div>
       <HeroTitleView />
+      <HeroRightSideView />
       <HeroSloganView />
+      <HeroInfoTextView />
+      <HeroSponsorUsButtonView />
+      <HeroNavigateToBottomButton />
+      <HeroBackdropView />
     </>
   );
 };
 
 export default HeroView;
+
+/*
+<HeroTitleView />
+<HeroSloganView />
+*/
