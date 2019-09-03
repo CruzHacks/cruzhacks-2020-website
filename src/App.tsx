b40a439a83bf15c2dcc742f186d19ed2ccc5b95f
Mocklee <emockler@ucsc.edu>
2019-09-02 18:47:08 -0700

    abstracted landing page further via landing.views (component layout for landing)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Hero from './components/hero/hero.view';
+import LandingViews from './components/landing/landing.views';
 
 const App: React.FC = () => {
   return (
     <>
-      <Hero />
+      <LandingViews />
     </>
   );
 };
