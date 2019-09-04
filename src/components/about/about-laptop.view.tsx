d2903c73657fbf6fe931ea2d3b8d0abf1cf827e4
tiluong <tiluong@ucsc.edu>
2019-09-04 00:02:42 -0700

    added views - will rename placeholder obj later on

---
+++ src/components/about/about-laptop.view.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './about.sass';
+
+const AboutLaptopView: React.FC = () => {
+    return (
+        <svg
+            width="421"
+            height="446"
+            viewBox="0 0 421 446"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <rect
+                x="-17.0148"
+                width="437.379"
+                height="446"
+            />
+        </svg>
+    );
+};
+
+export default AboutLaptopView;
