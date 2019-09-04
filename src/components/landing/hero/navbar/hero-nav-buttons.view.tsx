5c858305dc38263f7fcbdfdc22ba2cf23557e2b5
Eric Mockler <emockler@ucsc.edu>
2019-09-04 14:24:30 -0700

    finished cherrypicking merge to establish boilerplate

--- src/components/landing/hero/navbar/hero-nav-buttons.view.tsx
+++ src/components/landing/hero/navbar/hero-nav-buttons.view.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD:src/components/hero/navbar/hero-nav-buttons.view.tsx
-import './navbar.sass';
-import '../hero.sass';
-=======
->>>>>>> 9502555... Styles centralized to assets/styles folder with a base.scss file that imports all styles to be accessed globally. Refactored code for landing page. Landing page is still WIP, but I'm happy with the overall app structure at this point. Ready to merge with master.:src/components/landing/hero/navbar/hero-nav-buttons.view.tsx
 
 const HeroNavButtonsView: React.FC = () => {
   return (
--- src/components/landing/hero/navbar/hero-navbar.view.tsx
+++ src/components/landing/hero/navbar/hero-navbar.view.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import HeroNavButtonsView from './hero-nav-buttons.view';
-<<<<<<< HEAD:src/components/hero/navbar/hero-navbar.view.tsx
-import './navbar.sass';
-=======
->>>>>>> 9502555... Styles centralized to assets/styles folder with a base.scss file that imports all styles to be accessed globally. Refactored code for landing page. Landing page is still WIP, but I'm happy with the overall app structure at this point. Ready to merge with master.:src/components/landing/hero/navbar/hero-navbar.view.tsx
 
 const HeroNavbarView: React.FC = () => {
   return <HeroNavButtonsView />;
