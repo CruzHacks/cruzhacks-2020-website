a968808605908a977f4791d15398b5de69024481
Sean O'Hearn <seohearn1@gmail.com>
2019-09-07 12:28:56 -0700

    Working on the right side hero svg and responsiveness. Removed hero-slogan in favor of having it as part of the title container.

--- src/components/landing/hero/title-area/hero-title-area.view.tsx
+++ src/components/landing/hero/title-area/hero-title-area.view.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import HeroTitleView from './hero-title.view';
-import HeroSloganView from './hero-slogan.view';
 import HeroEventInfoTextView from './hero-event-info-text.view';
 import HeroSponsorUsButtonView from './hero-sponsor-us-button.view';
 import HeroNavigateToBottomButtonView from './hero-navigate-to-bottom-button';
@@ -9,7 +8,6 @@ const HeroTitleAreaView: React.FC = () => {
   return (
     <div className="hero-title-area-view-container">
       <HeroTitleView />
-      <HeroSloganView />
       <HeroEventInfoTextView />
       <HeroSponsorUsButtonView />
       <HeroNavigateToBottomButtonView />
