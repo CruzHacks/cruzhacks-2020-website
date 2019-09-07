a248fe890b86903aa1a70e012fd335622af98816
Sean O'Hearn <seohearn1@gmail.com>
2019-09-07 10:33:34 -0700

    Added a mixins scss file imported by base.scss. Some minor adjustments to landing container. Demonstrating how different sections can be added to one another without overlap.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import LandingView from './components/landing/landing.view';
+import AboutView from './components/about/about.view';
 
 const App: React.FC = () => {
   return (
     <div className="app-root">
       <LandingView />
+      <AboutView />
     </div>
   );
 };
 
 export default App;
+
+// <div className="test-container">Howdy Partner</div>
