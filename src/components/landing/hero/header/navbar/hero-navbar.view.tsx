d9a3b8f629d0fd1212fe5ff2c33b2216cd6e4be6
Sean O'Hearn <seohearn1@gmail.com>
2019-09-07 18:42:38 -0700

    Experimenting with different flex flowx. Converted svg to text for navbar. Refactored Hero header section. Moved fonts back to styles dir.

---
+++ src/components/landing/hero/header/navbar/hero-navbar.view.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import HeroNavButtonsView from './hero-nav-buttons.view';
+
+const HeroNavbarView: React.FC = () => {
+  return (
+    <div className="navbar__button-container">
+      <div className="navbar__button">
+        <p className="navbar__button__text">APPLY</p>
+      </div>
+      <div className="navbar__button">
+        <p className="navbar__button__text">SPONSOR</p>
+      </div>
+      <div className="navbar__button">
+        <p className="navbar__button__text">JOIN US</p>
+      </div>
+      <div className="navbar__button">
+        <p className="navbar__button__text">TEAM</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroNavbarView;
