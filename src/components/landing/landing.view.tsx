10616432f4210fdf9b90a8edf6d48b246b2c4e01
Mocklee <emockler@ucsc.edu>
2019-09-08 21:19:14 -0700

    added stat component

--- src/components/landing/landing.view.tsx
+++ src/components/landing/landing.view.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeroView from './hero/hero.view';
+import StatView from './stat/stat.view';
 
 const LandingViews: React.FC = () => {
   return (
     <>
       <div className="landing">
         <HeroView />
+        <StatView />
       </div>
     </>
   );
