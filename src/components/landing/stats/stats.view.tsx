790c1110ed65a46f3c9a4d5c4bc127899b69b104
mocklee <emockler@ucsc.edu>
2019-09-09 21:14:16 -0700

    moved styles to root /src dir to reduce nesting

--- src/components/landing/stats/stats.view.tsx
+++ src/components/landing/stats/stats.view.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import StatsTreeView from './tree/stats-tree.view';
 import StatsBoxView from './box/stats-box.view';
 
+const categories: string[] = [
+  'attendees',
+  'first time hackers',
+  'projects',
+  '$ in prizes',
+];
+
+const stats: string[] = ['500+', '47%', '80+', '10,000+'];
+
 const StatsView: React.FC = () => {
   return (
     <>
