84867160b19c4d05921133e92d22bcd07f5b79a2
mocklee <emockler@ucsc.edu>
2019-09-09 21:15:27 -0700

    cleaned up tests

--- src/tests/stats/landing.view.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import StatView from '../.../../../components/landing/stat/stat.view';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<StatView />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
