544da2a3287e9e6f972a49d2adca44db608f152b
tiluong <tiluong@ucsc.edu>
2019-09-11 01:21:17 -0700

    file cleanup

--- src/components/hero/navbar/hero-navbar.view.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-var hero_nav_buttons_view_1 = require("./hero-nav-buttons.view");
-require("./navbar.sass");
-var HeroNavbarView = function () {
-    return react_1["default"].createElement(hero_nav_buttons_view_1["default"], null);
-};
-exports["default"] = HeroNavbarView;
