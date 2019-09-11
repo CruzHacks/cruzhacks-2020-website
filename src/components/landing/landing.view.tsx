9e3aac9beb09a4e7fca55fda99aadd9e597f1da4
mocklee <emockler@ucsc.edu>
2019-09-10 19:49:39 -0700

    fixed rendering

--- src/components/landing/landing.view.tsx
+++ src/components/landing/landing.view.tsx
@@ -6,23 +6,17 @@ import StatsView from './stats/stats.view';
 import AboutView from './about/about.view';
 
 type LandingProps = {
-  componentText: {
+  text: {
     name: string;
     props: any;
   }[];
 };
 
 const LandingView: React.FC<LandingProps> = props => {
-  let componentImport = c =>
-    ({
-      HeroView: <HeroView {...c.props} />,
-      AboutView: <AboutView {...c.props} />,
-      StatsView: <StatView {...c.props} />,
-    }[c.name]);
   return (
     <>
       <div className="landing">
-        {props.componentText.slice(0).map(c => componentImport(c))}
+        <HeroView />
       </div>
     </>
   );
