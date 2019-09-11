ab69267ee59612e935aed30a177abf1cdf65a33e
tiluong <tiluong@ucsc.edu>
2019-09-11 13:12:24 -0700

    add test

--- src/components/about/about.test.tsx
+++ src/components/about/about.test.tsx
@@ -1,11 +1,9 @@
-
-// todo - uncomment this
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import About from './about.view';
+import About from './about.view';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<About />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<About />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
