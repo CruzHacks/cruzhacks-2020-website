5b193c1a0f85a6dcc962087cb368defb948630c0
tiluong <tiluong@ucsc.edu>
2019-09-11 01:21:17 -0700

    file cleanup

--- src/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-var hero_view_1 = require("./components/hero/hero.view");
-var App = function () {
-    return (react_1["default"].createElement(react_1["default"].Fragment, null,
-        react_1["default"].createElement(hero_view_1["default"], null)));
-};
-exports["default"] = App;
--- src/components/landing/landing.view.tsx
+++ src/components/landing/landing.view.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
-
-// import landing views
 import HeroView from './hero/hero.view';
-import StatsView from './stats/stats.view';
-import AboutView from './about/about.view';
 
-type LandingProps = {
-  componentText: {
-    name: string;
-    props: any;
-  }[];
-};
-
-const LandingView: React.FC<LandingProps> = props => {
-  let componentImport = c =>
-    ({
-      HeroView: <HeroView {...c.props} />,
-      AboutView: <AboutView {...c.props} />,
-      StatsView: <StatView {...c.props} />,
-    }[c.name]);
+const LandingView: React.FC = () => {
   return (
-    <>
-      <div className="landing">
-        {props.componentText.slice(0).map(c => componentImport(c))}
-      </div>
-    </>
+    <div className="landing-view-container">
+      <HeroView />
+    </div>
   );
 };
 
