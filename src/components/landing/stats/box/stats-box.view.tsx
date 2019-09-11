ddc58d308cc0024b7e3e1952708e8d8f16234066
mocklee <emockler@ucsc.edu>
2019-09-11 14:00:15 -0700

    added containers to about section

--- src/components/landing/stats/box/stats-box.view.tsx
+++ src/components/landing/stats/box/stats-box.view.tsx
@@ -1,19 +1,19 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 
-type BoxProps = {
-  stats: {
-    category: string;
-    stat: string;
-  }[];
-};
+interface BoxProps {
+  [index: string]: { category: string; stat: string }[];
+}
 
 export const StatsBoxView: React.FC<BoxProps> = BoxProps => {
   return (
     <>
-      {/* BoxProps.stats.map(metric => ) */}
-      <div className="stats__box">
-        <span className="stats__stat">{}</span>
-        <span className="stats__category">{}</span>
+      <div className="stats__box-container">
+        {BoxProps.stats.map(metric => (
+          <div className="stats__box">
+            <span className="stats__stat">{metric.stat}</span>
+            <span className="stats__category">{metric.category}</span>
+          </div>
+        ))}
       </div>
     </>
   );
