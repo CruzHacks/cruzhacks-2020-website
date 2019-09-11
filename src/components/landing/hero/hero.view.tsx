6b22b7cabdaa8411395116bdc22f27881734f49b
mocklee <emockler@ucsc.edu>
2019-09-11 14:00:15 -0700

    added containers to about section

--- src/components/landing/hero/hero.view.tsx
+++ src/components/landing/hero/hero.view.tsx
@@ -7,10 +7,12 @@ import HeroBackdropView from './backdrop/hero-backdrop.view';
 const HeroView: React.FC = () => {
   return (
     <>
-      <HeroHeaderView />
-      <HeroTitleAreaView />
-      <HeroRightSideView />
-      <HeroBackdropView />
+      <section className="hero__container">
+        <HeroHeaderView />
+        <HeroTitleAreaView />
+        <HeroRightSideView />
+        <HeroBackdropView />
+      </section>
     </>
   );
 };
--- src/components/landing/stats/stats.view.tsx
+++ src/components/landing/stats/stats.view.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 import StatsTreeView from './tree/stats-tree.view';
 import StatsBoxView from './box/stats-box.view';
 
-let stats: [
+let stats = [
   {
-    category: 'attendees';
-    stat: '500+';
+    category: 'attendees',
+    stat: '500+',
   },
   {
-    category: 'first time hackers';
-    stat: '47%';
+    category: 'first time hackers',
+    stat: '47%',
   },
   {
-    category: 'projects';
-    stat: '80+';
+    category: 'projects',
+    stat: '80+',
   },
   {
-    category: '$ in prizes';
-    stat: '10,000+';
-  }
+    category: '$ in prizes',
+    stat: '10,000+',
+  },
 ];
 
 const StatsView: React.FC = () => {
@@ -27,7 +27,7 @@ const StatsView: React.FC = () => {
       <div className="stats__container">
         <h1 className="stats__title">Milestones of 2019</h1>
         <StatsTreeView />
-        <StatsBoxView stats={stats} />
+        <StatsBoxView stats={stats} />}
       </div>
     </>
   );
