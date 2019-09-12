e76c282532005922e544fbce8c7443782197e9bd
mocklee <emockler@ucsc.edu>
2019-09-12 12:21:01 -0700

    updated fonts, added styles for stat boxes

--- src/components/landing/stats/tree/stats-tree.view.tsx
+++ src/components/landing/stats/tree/stats-tree.view.tsx
@@ -4,12 +4,7 @@ export const StatsTree: React.FC = () => {
   return (
     <>
       <div className="stats__tree-container">
-        <svg
-          className="stats__tree"
-          viewBox="0 0 618 169"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg className="stats__tree" viewBox="0 0 618 169" fill="none">
           <path
             d="M7.3 158.488v-52.01c0-9.687 7.866-17.53 17.582-17.53h572.291c7.461 0 13.528 6.032 13.528 13.487v62.522M206.243 88.947l.568 69.541M409.567 158.488V89.643M309 88.947V14.555"
             stroke="#232323"
