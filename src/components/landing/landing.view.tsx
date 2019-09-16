460855ba16ea0a24c14a477ebac635ac2739718f
Isai Lopez <ilopezro@ucsc.edu>
2019-09-16 16:19:15 -0700

    added component to landing page

--- src/components/landing/landing.view.tsx
+++ src/components/landing/landing.view.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeroView from './hero/hero.view';
+import FAQView from '../faq/faq.view'
 
 const LandingViews: React.FC = () => {
   return (
     <>
       <div className="landing">
-        <HeroView />
+        <HeroView/>
+        <FAQView />
       </div>
     </>
   );
