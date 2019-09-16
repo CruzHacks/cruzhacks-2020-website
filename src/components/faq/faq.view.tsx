45754b17e14e64a25189ff6327dbd272b1c746fc
Isai Lopez <ilopezro@ucsc.edu>
2019-09-16 16:19:56 -0700

    created faq view file

---
+++ src/components/faq/faq.view.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FAQRectangle from './container/faq-container.view'
+
+
+const FAQView: React.FC = () => {
+    return (
+      <>
+      <div className="faq__container">
+        <FAQRectangle/>
+      </div>
+      </>
+    )
+}
+
+export default FAQView
