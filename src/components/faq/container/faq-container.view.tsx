0b1441c7c047788e3f4e04bf586eb21c0c36378d
Isai Lopez <ilopezro@ucsc.edu>
2019-09-16 16:20:07 -0700

    created rectangle container file

---
+++ src/components/faq/container/faq-container.view.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FAQRectangle: React.FC = () => {
+    return (
+      <>
+      <div className="faq-rectangle">
+        <svg width="804" height="617" viewBox="0 0 804 617" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M77 2h649.697c41.421 0 75 33.579 75 75v463c0 41.421-33.579 75-75 75H77c-41.421 0-75-33.579-75-75V77C2 35.579 35.579 2 77 2z" stroke="#7171CB" strokeWidth="4"/>
+        </svg>
+      </div>
+      </>
+    )
+}
+
+export default FAQRectangle
