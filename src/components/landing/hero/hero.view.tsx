d75d916b4dba39a31a665a08e151b99a10d348ba
Sean O'Hearn <seohearn1@gmail.com>
2019-09-18 12:32:49 -0700

    Almost finished with initial cta view. Adjustements to hero view. Using preserve aspect ratio on svgs, and relying more on percentages for reactive views.

--- src/components/landing/hero/hero.view.tsx
+++ src/components/landing/hero/hero.view.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import HeroHeaderView from './header/hero-header.view';
 import HeroTitleAreaView from './title-area/hero-title-area.view';
-// import HeroRightSideView from './backdrop/hero-rightside.view';
-// import HeroBackdropView from './backdrop/hero-backdrop.view';
+import HeroRightSideView from './backdrop/hero-rightside.view';
+import HeroBackdropView from './backdrop/hero-backdrop.view';
 
 const HeroView: React.FC = () => {
   return (
-    <>
+    <div className="hero-view-container">
       <HeroHeaderView />
       <HeroTitleAreaView />
-    </>
+      <HeroRightSideView />
+      <HeroBackdropView />
+    </div>
   );
 };
 
