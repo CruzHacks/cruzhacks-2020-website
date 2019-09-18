85af5758cd1e2c38757a6df8ff993414f3ee1a74
mrunknown.892@gmail.com <mrunknown.892@gmail.com>
2019-09-18 02:38:32 -0700

    refactored footer backdrop to better position footer's content

--- src/components/footer/footer.view.tsx
+++ src/components/footer/footer.view.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 // import FooterBackdropView from './backdrop/footer-backdrop.view';
-import FooterTextView from './text/footer-text.view';
-import FooterButtonView from './buttons/footer-button.view';
+// import FooterTextView from './text/footer-text.view';
+// import FooterButtonView from './buttons/footer-button.view';
+
+import FooterContentView from './content/footer-content.view';
 
 const FooterView : React.FC = () => {
     return (
-        <div className="footer-view-container">
-            <FooterButtonView/>    
-            <FooterTextView/>
+        <div className="footer-view__container">
+            <FooterContentView/>
         </div>
     );
 };
