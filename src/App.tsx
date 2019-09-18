8be48f5c6a8425bc31caf07dc0829152c65beb7b
Sean O'Hearn <seohearn1@gmail.com>
2019-09-18 12:32:49 -0700

    Almost finished with initial cta view. Adjustements to hero view. Using preserve aspect ratio on svgs, and relying more on percentages for reactive views.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import LandingView from './components/landing/landing.view';
-import AboutView from './components/about/about.view';
 import WelcomeView from './components/welcome/welcome.view';
 import FooterView from './components/footer/footer.view';
 import CtaView from './components/cta/cta.view';
@@ -9,7 +8,6 @@ const App: React.FC = () => {
   return (
     <>
       <LandingView />
-      <AboutView />
       <WelcomeView />
       <CtaView />
       <FooterView />
@@ -18,5 +16,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-// <div className="test-container">Howdy Partner</div>
--- src/components/cta/cta.view.tsx
+++ src/components/cta/cta.view.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import JoinOurMissionView from './join-our-mission.view';
-import SponsorUsView from './sponsor-us.view';
+import JoinOurMissionView from './cta-join-our-mission.view';
+import SponsorUsView from './cta-sponsor-us.view';
+import CtaBackdropView from './backdrop/cta-backdrop.view';
 
 const CtaView: React.FC = () => {
   return (
     <div className="cta-view-container">
       <JoinOurMissionView />
       <SponsorUsView />
+      <CtaBackdropView />
     </div>
   );
 };
