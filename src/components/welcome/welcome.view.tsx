181df2fc9fe77da00b1261c2879b6d455fbbe11c
mrunknown.892@gmail.com <mrunknown.892@gmail.com>
2019-09-17 19:11:57 -0700

    commented all backdrop views in preparation for background component

--- src/components/welcome/welcome.view.tsx
+++ src/components/welcome/welcome.view.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import WelcomeTitleView from './title/welcome-title.view';
 import WelcomeCardsView from './cards/welcome-cards.view';
-import WelcomeBackdropView from './backdrop/welcome-backdrop.view';
+
+// import WelcomeBackdropView from './backdrop/welcome-backdrop.view';
 
 const WelcomeView: React.FC = () => {
     return (
         <div className="welcome-view-container">
-                <WelcomeBackdropView />
                 <WelcomeTitleView />
                 <WelcomeCardsView/>
         </div>
