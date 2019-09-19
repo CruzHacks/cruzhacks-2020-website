8fb73feb109127b10709e7e8079155f1077d1ae0
Isai Lopez <ilopezro@ucsc.edu>
2019-09-19 13:50:45 -0700

    created popup view

---
+++ src/components/faq/popup/faq-popup.view.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type AnswerProps = {
+    question: string; 
+    answer: string; 
+    close(): any; 
+}
+
+export const FAQPopupView: React.FC<AnswerProps> = AnswerProps => {
+    return(
+        <div className="faq__popupContainer">
+            <div className="faq__popupInner">
+                <span className="faq__popupQuestion">{AnswerProps.question}</span>
+                <span className="faq__popupAnswer">{AnswerProps.answer}</span>
+                <button className="faq__popupExit" onClick={AnswerProps.close}></button>
+            </div>
+        </div>
+    )
+}
+
+export default FAQPopupView;
