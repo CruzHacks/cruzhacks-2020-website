015b29e99c0a8d7fb872e23356b2fb66f871a328
mrunknown.892@gmail.com <mrunknown.892@gmail.com>
2019-09-18 20:06:32 -0700

    refactored svg code to get rid of warning messages

--- src/components/background/backdrop/footer/backdrop-footer-extended.view.tsx
+++ src/components/background/backdrop/footer/backdrop-footer-extended.view.tsx
@@ -6,12 +6,12 @@ const BackdropFooterExtendedView: React.FC = () => {
             <path d="M392.4 1501.01L827.368 1267.17C840.244 1260.24 840.236 1240.76 827.36 1233.84L714.08 1173.02C699.978 1165.45 699.986 1144.11 714.096 1136.54L1173.02 890.67V142.824L1136.28 123.004C1104.61 105.919 1067.14 105.885 1035.45 122.91L680.074 313.76L116.288 11.9723C87.1953 -3.59995 52.9232 -4.00165 23.508 10.8869L-21.0182 33.4248V1300.06L352.981 1501.01C365.365 1507.66 380.016 1507.66 392.4 1501.01Z" fill="url(#footer-extended_paint0_linear)" />
             <defs>
                 <linearGradient id="footer-extended_paint0_linear" x1="137.136" y1="206.037" x2="1027.97" y2="1138.27" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#EBF7FB" />
-                    <stop offset="0.229167" stop-color="#EDEDFF" />
-                    <stop offset="0.421875" stop-color="#DBDBFF" />
-                    <stop offset="0.5533" stop-color="#BEBEFF" />
-                    <stop offset="0.797" stop-color="#9595FF" />
-                    <stop offset="1" stop-color="#6E6EFF" />
+                    <stop stopColor="#EBF7FB" />
+                    <stop offset="0.229167" stopColor="#EDEDFF" />
+                    <stop offset="0.421875" stopColor="#DBDBFF" />
+                    <stop offset="0.5533" stopColor="#BEBEFF" />
+                    <stop offset="0.797" stopColor="#9595FF" />
+                    <stop offset="1" stopColor="#6E6EFF" />
                 </linearGradient>
             </defs>
         </svg>
