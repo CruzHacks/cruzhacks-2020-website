1e325862ca531d7116682b21eb6e871e52a71f1b
Sean O'Hearn <seohearn1@gmail.com>
2019-09-19 21:23:45 -0700

    Some refactoring of the Hero component and the CTA component. Using font mixins, had to fix windows relative url issue by creating an App.scss file and importing base from it. Now index.scss just includes the global styles for body and root. Merged the latest from CTA component.

--- src/components/cta/cta-sponsor-us.view.tsx
+++ src/components/cta/cta-sponsor-us.view.tsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 const SponsorUsView: React.FC = () => {
   return (
-    <div className="sponsor-us-container">
-      <div className="sponsor-us-container__title">
-        <h1 className="sponsor-us-container__main">SPONSOR US</h1>
+    <div className="sponsor-us">
+      <div className="sponsor-us__title">
+        <h1 className="sponsor-us__title-text">SPONSOR US</h1>
       </div>
-      <div className="sponsor-us-container__description">
-        <p className="sponsor-us-container__body">
+      <div className="sponsor-us__description">
+        <p className="sponsor-us__description-text">
           Without the help of our sponsors Cruzhacks simply would
         </p>
-        <p className="sponsor-us-container__body">
+        <p className="sponsor-us__description-text">
           not be possible. If you're interested in becoming a sponsor
         </p>
-        <p className="sponsor-us-container__body">
+        <p className="sponsor-us__description-text">
           for Cruzhacks 2020 then please register your interest below.
         </p>
       </div>
-      <div className="sponsor-us-container__button-container">
-        <div className="sponsor-us-container__button">
-          <p className="button__text">LEARN MORE</p>
+      <div className="sponsor-us__button-container">
+        <div className="sponsor-us__button">
+          <p className="sponsor-us__button-text">LEARN MORE</p>
         </div>
       </div>
     </div>
