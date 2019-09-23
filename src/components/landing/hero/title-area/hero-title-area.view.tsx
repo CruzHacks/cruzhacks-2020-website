ac3c551cb35736fce5a9d13b238b60014cab4b73
Sean O'Hearn <seohearn1@gmail.com>
2019-09-22 22:16:29 -0700

    Added responsive font mixin, responsive layout for title area up to small mobile viewport size.

--- src/components/landing/hero/title-area/hero-title-area.view.tsx
+++ src/components/landing/hero/title-area/hero-title-area.view.tsx
@@ -4,18 +4,16 @@ import HeroNavigateToBottomButtonView from './hero-navigate-to-bottom-button.vie
 const HeroTitleAreaView: React.FC = () => {
   return (
     <div className="hero-title-area">
-      <div className="hero-title-area__title">
-        <h1 className="hero-title-area__title-text">cruzhacks 2020</h1>
-      </div>
-      <div className="hero-title-area__slogan">
-        <p className="hero-title-area__slogan-text">you.belongHere();</p>
-      </div>
-      <div className="hero-title-area__eventInfo">
-        <div className="hero-title-area__eventInfo-text">
-          <p>// January 19-21st, 2020</p>
-          <p>// Stevenson Event Center, UC Santa Cruz</p>
-          <p>// Applications Release Nov.</p>
+      <div className="hero-title-area__title-text">
+        cruzhacks 2020
+        <div className="hero-title-area__slogan hero-title-area__slogan-text">
+          you.belongHere();
         </div>
+      </div>
+      <div className="hero-title-area__eventInfo hero-title-area__eventInfo-text">
+        <p>// January 19-21st, 2020</p>
+        <p>// Stevenson Event Center, UC Santa Cruz</p>
+        <p>// Applications Release Nov.</p>
       </div>
       <div className="hero-title-area__button">
         <p className="hero-title-area__button-text">SPONSOR US</p>
