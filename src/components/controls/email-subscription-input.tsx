2256724adee213ebc4838c4b4bad911794af9809
Sean O'Hearn <seohearn1@gmail.com>
2019-09-24 15:41:36 -0700

    fixed CORS issue for mailchimp requests. improving input validation. about to implement tooltips.

--- src/components/controls/email-subscription-input.tsx
+++ src/components/controls/email-subscription-input.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios, { AxiosRequestConfig } from 'axios';
 
+const emailInputRef: any = React.createRef();
+
 export interface ITextInputProps {
   placeholder: string;
   value: string;
@@ -8,49 +10,54 @@ export interface ITextInputProps {
   containerClass: string;
 }
 
+function validateInputSubmission(event: any): boolean {
+  console.log(event);
+  return true;
+}
+
 async function subscribeToEmailList(event: any, email: string) {
-  let username: string = '' + process.env.REACT_APP_MAILCHIMP_USER;
-  let password: string = '' + process.env.REACT_APP_MAILCHIMP_SECRET;
-  let mailchimpEndpoint: string =
-    '' + process.env.REACT_APP_MAILCHIMP_SUBSCRIBERS_ENDPOINT;
+  if (validateInputSubmission(event)) {
+    var proxy = 'https://cors-anywhere.herokuapp.com/';
 
-  let axiosConfig: AxiosRequestConfig = {
-    params: {
-      email_address: email,
-      status: 'subscribed',
-    },
-    auth: { username, password },
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
-      'Content-Type': 'application/json',
-    },
-    proxy: {
-      host: '127.0.0.1',
-      port: 9001,
-    },
-  };
+    let username: string = '' + process.env.REACT_APP_MAILCHIMP_USER;
+    let password: string = '' + process.env.REACT_APP_MAILCHIMP_SECRET;
+    let mailchimpEndpoint: string =
+      proxy + process.env.REACT_APP_MAILCHIMP_SUBSCRIBERS_ENDPOINT;
 
-  try {
-    const response = await axios.get(mailchimpEndpoint, axiosConfig);
-    console.log(response);
-  } catch (error) {
-    if (error.response && error.response.status === 400) {
-      if (error.response.data.title === 'Member Exists') {
-        let err_msg = 'Already subscribed!';
-        console.error(error);
-        console.error(err_msg);
-      } else if (
-        error.response.data.title === 'Forgotten Email Not Subscribed'
-      ) {
-        let err_msg = "Previously unsubscribed! Can't add email :(";
+    let axiosConfig: AxiosRequestConfig = {
+      params: {
+        email_address: email,
+        status: 'subscribed',
+      },
+      auth: { username, password },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const response = await axios.post(mailchimpEndpoint, axiosConfig);
+      console.log(response);
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        if (error.response.data.title === 'Member Exists') {
+          let err_msg = 'Already subscribed!';
+          console.error(error);
+          console.error(err_msg);
+        } else if (
+          error.response.data.title === 'Forgotten Email Not Subscribed'
+        ) {
+          let err_msg = "Previously unsubscribed! Can't add email :(";
+          console.error(error);
+          console.error(err_msg);
+        }
+      } else {
+        let err_msg = 'Something Went Wrong';
         console.error(error);
         console.error(err_msg);
       }
-    } else {
-      let err_msg = 'Something Went Wrong';
-      console.error(error);
-      console.error(err_msg);
     }
   }
 }
@@ -67,16 +74,21 @@ const EmailSubscriptionInput: React.FC<
     return (
       <div className={textInputData.containerClass}>
         <input
+          ref={emailInputRef}
+          id="emailInput"
           type="email"
           placeholder={textInputData.placeholder}
           value={textInputData.value}
           onChange={e =>
             setTextInputData({ ...textInputData, value: e.target.value })
           }
+          required
         />
-        <button onClick={e => subscribeToEmailList(e, textInputData.value)}>
-          {textInputData.buttonText}
-        </button>
+        <input
+          type="submit"
+          onClick={e => subscribeToEmailList(e, textInputData.value)}
+          value={textInputData.buttonText}
+        ></input>
       </div>
     );
   } else {
