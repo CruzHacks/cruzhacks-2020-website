df231a881735816cf516ea213e87c134803bb1e9
Sean O'Hearn <seohearn1@gmail.com>
2019-09-24 15:53:35 -0700

    added routes from master for deployment.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import LandingView from './components/landing/landing.view';
-import WelcomeView from './components/welcome/welcome.view';
-import FooterView from './components/footer/footer.view';
-import CtaView from './components/cta/cta.view';
+import Routes from './routes/routes';
 import './App.scss';
 
 const App: React.FC = () => {
   return (
     <>
-      <LandingView />
-      <WelcomeView />
-      <CtaView />
-      <FooterView />
+      <Routes />
     </>
   );
 };
