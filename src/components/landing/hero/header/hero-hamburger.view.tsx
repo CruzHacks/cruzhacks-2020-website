35baed33ee1893664c60c49fcc54491c36b8b4b0
mocklee <emockler@ucsc.edu>
2019-09-25 13:26:46 -0700

    hero & stats are prod-ready

--- src/components/landing/hero/header/hero-hamburger.view.tsx
+++ src/components/landing/hero/header/hero-hamburger.view.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const HeroHamburgerView: React.FC = () => {
   return (
     <div className="hamburger">
+      {/* 
       <svg className="hamburger__svg" viewBox="0 0 24 15" fill="none">
         <path
           stroke="#020202"
@@ -11,7 +12,7 @@ const HeroHamburgerView: React.FC = () => {
           strokeLinejoin="round"
           d="M1.5 1.5h21M1.5 7.5h14M1.5 13.5h21"
         />
-      </svg>
+      </svg> */}
     </div>
   );
 };
