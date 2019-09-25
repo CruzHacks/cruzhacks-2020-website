e3327dd6b5d3bee1d5571d8f34d36192b97f956c
mocklee <emockler@ucsc.edu>
2019-09-25 00:39:46 -0700

    fixed svg tags

--- src/components/landing/hero/header/hero-hamburger.view.tsx
+++ src/components/landing/hero/header/hero-hamburger.view.tsx
@@ -6,9 +6,9 @@ const HeroHamburgerView: React.FC = () => {
       <svg className="hamburger__svg" viewBox="0 0 24 15" fill="none">
         <path
           stroke="#020202"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M1.5 1.5h21M1.5 7.5h14M1.5 13.5h21"
         />
       </svg>
