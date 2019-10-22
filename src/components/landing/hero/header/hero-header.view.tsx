a55598b9da51297ed02184b3d4623a5b75c5ee97
sohearn <sohearn@ucsc.edu>
2019-10-22 11:06:53 -0700

    using percentage on badge so mobile view is consistent, and I've adjusted the position of the lightbulb.

--- src/components/landing/hero/header/hero-header.view.tsx
+++ src/components/landing/hero/header/hero-header.view.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import HeroLightBulb from './hero-lightbulb.view';
 // import HeroNavbarView from './navbar/hero-navbar.view';
 import HeroHamburgerView from './hero-hamburger.view';
-import HeroMlhBadgeView from './hero-mlh-badge.view';
+// import HeroMlhBadgeView from './hero-mlh-badge.view';
 
 const HeroHeaderView: React.FC = () => {
   return (
     <div className="hero__header">
       <HeroLightBulb />
-      <HeroMlhBadgeView />
       <HeroHamburgerView />
     </div>
   );
