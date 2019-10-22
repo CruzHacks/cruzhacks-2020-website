9c6d64b5451faccde48a9e38153462064e5cecb3
Eric Mockler <emockler@ucsc.edu>
2019-10-21 17:27:29 -0700

    fixed FAQ divider unit

--- src/components/landing/sponsors/container/sponsors-container.view.tsx
+++ src/components/landing/sponsors/container/sponsors-container.view.tsx
@@ -8,7 +8,6 @@ const SponsorsContainer: React.FC = () => {
       <span className="sponsors__title">Thank You Sponsors</span>
       <span className="sponsors__divider"></span>
       <span className="sponsors__logosContainer">
-        <img src={logos} className="sponsors__logos"></img>
       </span>
     </div>
   );
