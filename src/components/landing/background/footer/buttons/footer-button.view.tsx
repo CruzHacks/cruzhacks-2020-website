984eb2c62e0bd30689d48918d0e648c3855e91bb
mocklee <emockler@ucsc.edu>
2019-11-07 13:29:24 -0800

    fixed footer links

--- src/components/landing/background/footer/buttons/footer-button.view.tsx
+++ src/components/landing/background/footer/buttons/footer-button.view.tsx
@@ -39,7 +39,10 @@ const FooterButtonView: React.FC = () => {
       <div className="footer-up__button">
         <svg
           className="footer-up__image"
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+            console.log('didit');
+          }}
           width="50"
           height="49"
           viewBox="0 0 50 49"
