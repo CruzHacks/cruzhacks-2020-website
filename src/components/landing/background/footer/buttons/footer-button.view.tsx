8c9fe3a9f40489ee35c6cc2fab00c62c9ba69945
mrunknown.892@gmail.com <mrunknown.892@gmail.com>
2019-11-07 22:39:28 -0800

    issues with z-index

--- src/components/landing/background/footer/buttons/footer-button.view.tsx
+++ src/components/landing/background/footer/buttons/footer-button.view.tsx
@@ -39,7 +39,7 @@ const FooterButtonView: React.FC = () => {
       <div className="footer-up__button">
         <svg
           className="footer-up__image"
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+          onClick={() => window.scrollTo(0,0)}
           width="50"
           height="49"
           viewBox="0 0 50 49"
