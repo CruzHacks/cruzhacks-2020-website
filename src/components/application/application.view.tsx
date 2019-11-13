d387ca8a9f0fb37d7014f4cdd1083c42a79d7c22
mrunknown.892@gmail.com <mrunknown.892@gmail.com>
2019-11-13 00:01:00 -0800

    added firstname and lastname form inputs

--- src/components/application/application.view.tsx
+++ src/components/application/application.view.tsx
@@ -4,12 +4,20 @@ const ApplicationView: React.FC = () => {
     return(
         <>
             <div className="application">
-                <span className="application__header">Demographics</span>
+                <h3 className="application__header">Demographics</h3>
                 <form className="application__form">
+                <section>
+                    <div className="application__first-name">
+                        <label className="application__label">First Name</label>
+                        <input className="application__input" type="text"/>
+                    </div>
                 
-                
-                
-                
+                    <div className="application__last-name">
+                        <label className="application__label">Last Name</label>
+                        <input className="application__input" type="text"/>
+                    </div>
+                    <br style={{clear:'both'}} />
+                </section>
                 </form>
             </div>
         </>
