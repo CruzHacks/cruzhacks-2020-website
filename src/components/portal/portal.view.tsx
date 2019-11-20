e7d6ba4fbb8698126395d9b2c99c2b8c70a273df
mocklee <emockler@ucsc.edu>
2019-11-19 22:35:10 -0800

    added announcements component

--- src/components/portal/portal.view.tsx
+++ src/components/portal/portal.view.tsx
@@ -7,15 +7,25 @@ const PortalView: React.FC = () => {
   return (
     <>
       <div className="portal">
-        <div className="portal__titleBar">
+        <div className="portal__navbar">
           <HeroLightBulbView />
-          <div className="portal__titleBar-text-container">
-            <span className="portal__titleBar-title">Dashboard</span>
-            <span className="portal__titleBar-logout">Log out</span>
+          <div className="portal__navbar-text-container">
+            <span className="portal__navbar-title">Dashboard</span>
+            <span className="portal__navbar-logout">Log out</span>
           </div>
         </div>
-        <div className="portal__appStatus"></div>
-        <div className="portal__announcements"></div>
+        <div className="portal__appstatus">
+          <div className="portal__appstatus-container">
+            <span className="portal__appstatus-text">
+              Your application is in review. Days until CruzHacks:
+            </span>
+            <div className="portal__appstatus-status-container"></div>
+          </div>
+        </div>
+        <div className="portal__announcements">
+          <span className="portal__announcements-styletext">ANNOUNCEMENTS</span>
+          <div className="portal__announcements-box"></div>
+        </div>
         <div className="portal__application">
           <ApplicationView />
         </div>
