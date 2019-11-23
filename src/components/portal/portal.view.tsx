b339b7d52bed5b3c197c5029d296437b7183d9a4
Sean O'Hearn <seohearn1@gmail.com>
2019-11-22 20:57:39 -0800

    This pretty much does it for the auth0 integration on the front end. We have a private route wrapper that will constanly check if the user is authenticated using the useEffect() functionality. The user will click on APPLY to login and be redirected to out auth0 login page, and if authenticated will be sent to the portal, otherwise back to the landing page.

--- src/components/portal/portal.view.tsx
+++ src/components/portal/portal.view.tsx
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ApplicationView from '../application/application.view';
-import { NavLink } from 'react-router-dom';
 import HeroLightBulbView from '../landing/hero/header/hero-lightbulb.view';
+import { useAuth0 } from '../../auth/auth';
 
 const PortalView: React.FC = () => {
+  const authContext = useAuth0()!;
+
+  const { user, logout } = authContext;
+
+  const userName = user.name.split('@')[0];
+
+  const logoutWithRedirect = () =>
+    logout({
+      returnTo: window.location.origin,
+    });
+
   return (
     <>
       <div className="portal">
@@ -11,20 +22,28 @@ const PortalView: React.FC = () => {
           <HeroLightBulbView />
           <div className="portal__navbar-text-container">
             <span className="portal__navbar-title">Dashboard</span>
-            <span className="portal__navbar-logout">Log out</span>
+            <span
+              className="portal__navbar-logout"
+              onClick={() => logoutWithRedirect()}
+            >
+              Log out
+            </span>
           </div>
         </div>
         <div className="portal__appstatus">
           <div className="portal__appstatus-container">
             <span className="portal__appstatus-text">
-              Your application is under review. Days until CruzHacks:
+              Hi {userName}, your application is under review. Days until
+              CruzHacks:
             </span>
             <div className="portal__appstatus-status-container"></div>
           </div>
         </div>
         <div className="portal__announcements">
           <div className="portal__announcements-container">
-            <span className="portal__announcements-styletext">ANNOUNCEMENTS</span>
+            <span className="portal__announcements-styletext">
+              ANNOUNCEMENTS
+            </span>
             <div className="portal__announcements-box"></div>
           </div>
         </div>
