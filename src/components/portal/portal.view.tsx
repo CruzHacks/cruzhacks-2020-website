7f49bfa1033b4061a11c7700bb1fbe052da05e82
kyle1668 <kyledevinobrien1@gmail.com>
2019-11-23 17:46:54 -0800

    added functionality to check if the user has applied

--- src/components/portal/portal.view.tsx
+++ src/components/portal/portal.view.tsx
@@ -2,38 +2,78 @@ import React, { useState } from 'react';
 import ApplicationView from '../application/application.view';
 import { NavLink } from 'react-router-dom';
 import HeroLightBulbView from '../landing/hero/header/hero-lightbulb.view';
+import axios, { AxiosRequestConfig } from 'axios';
 
-const PortalView: React.FC = () => {
-  return (
-    <>
-      <div className="portal">
-        <div className="portal__navbar">
-          <HeroLightBulbView />
-          <div className="portal__navbar-text-container">
-            <span className="portal__navbar-title">Dashboard</span>
-            <span className="portal__navbar-logout">Log out</span>
+function applicationHasBeenSubmitted(email: string): Promise<boolean> {
+  const endpoint: string = process.env.REACT_APP_API_ENDPOINT + '';
+  const apiKey = process.env.REACT_APP_API_KEY + '';
+  const queryParams = `?authentication=${apiKey}&accountType=hacker&accountEmail=${email}`;
+
+  return axios
+    .get<Array<Object>>(endpoint + queryParams)
+    .then(response => {
+      console.log(response);
+      return response.data.length !== 0;
+    })
+    .catch(error => {
+      return error;
+    });
+}
+
+class PortalView extends React.Component {
+  state = {
+    applicationStatusMessage: '',
+  };
+
+  componentDidMount() {
+    applicationHasBeenSubmitted('kdobrien@ucsc.edu')
+      .then(hasSubmitted => {
+        const message = hasSubmitted === true
+          ? 'Your application is under review.'
+          : "You haven't yet submitted your application! Apply Here";
+        this.setState({ applicationStatusMessage: message });
+        console.log('ran');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="portal">
+          <div className="portal__navbar">
+            <HeroLightBulbView />
+            <div className="portal__navbar-text-container">
+              <span className="portal__navbar-title">Dashboard</span>
+              <span className="portal__navbar-logout">Log out</span>
+            </div>
+          </div>
+          <div className="portal__appstatus">
+            <div className="portal__appstatus-container">
+              <span className="portal__appstatus-text">
+                {/* Your application is under review. Days until CruzHacks: */}
+                {this.state.applicationStatusMessage}
+              </span>
+              {/* <div className="portal__appstatus-status-container"></div> */}
+            </div>
+          </div>
+          <div className="portal__announcements">
+            <div className="portal__announcements-container">
+              <span className="portal__announcements-styletext">
+                ANNOUNCEMENTS
+              </span>
+              <div className="portal__announcements-box"></div>
+            </div>
+          </div>
+          <div className="portal__application">
+            <ApplicationView />
           </div>
         </div>
-        <div className="portal__appstatus">
-          <div className="portal__appstatus-container">
-            <span className="portal__appstatus-text">
-              Your application is under review. Days until CruzHacks:
-            </span>
-            <div className="portal__appstatus-status-container"></div>
-          </div>
-        </div>
-        <div className="portal__announcements">
-          <div className="portal__announcements-container">
-            <span className="portal__announcements-styletext">ANNOUNCEMENTS</span>
-            <div className="portal__announcements-box"></div>
-          </div>
-        </div>
-        <div className="portal__application">
-          <ApplicationView />
-        </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default PortalView;
