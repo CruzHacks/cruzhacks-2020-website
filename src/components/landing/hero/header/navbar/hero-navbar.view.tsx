6c0ead186dd97decc9d20c3c35af8184c98002a9
mocklee <emockler@ucsc.edu>
2019-11-30 21:22:38 -0800

    improved click targets in navbar

--- src/components/landing/hero/header/navbar/hero-navbar.view.tsx
+++ src/components/landing/hero/header/navbar/hero-navbar.view.tsx
@@ -14,11 +14,12 @@ const HeroNavbarView: React.FC = () => {
 
   return (
     <div className="navbar">
-      <Link to="/portal" style={{ textDecoration: `none` }}>
-        <div className="navbar__button">
+      <div className="navbar__button">
+        <Link to="/portal" style={{ textDecoration: `none` }}>
           <p className="navbar__button-text">APPLY</p>
-        </div>
-      </Link>
+        </Link>
+      </div>
+
       <div
         onMouseEnter={showDropMenu}
         onMouseLeave={hideDropMenu}
