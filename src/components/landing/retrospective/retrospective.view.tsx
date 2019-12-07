3566225b21299a6407bc7b11ff4f9fbcb3427e5c
mocklee <emockler@ucsc.edu>
2019-12-07 01:38:34 -0800

    rendered all photo and quote elements in grid

--- src/components/landing/retrospective/retrospective.view.tsx
+++ src/components/landing/retrospective/retrospective.view.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import retroImage1 from '../../../assets/images/retrospective/THX_8296.png';
+import retroImage2 from '../../../assets/images/retrospective/THX_8336.png';
+import retroImage3 from '../../../assets/images/retrospective/THX_9456.png';
+import retroImage4 from '../../../assets/images/retrospective/THX_9446.png';
 import QuoteView from './quote/quote.view';
 
 const RetrospectiveView: React.FC = () => {
   return (
     <>
       <div className="retrospective__container">
-        <img className="retrospective__image 1" src={retroImage1} />
+        <img className="retrospective__image-1" src={retroImage1} />
         <QuoteView
-          quote="I'm happy to say that I am officially committing to pursue 
+          quote="The idea seemed difficult to accomplish in 36 hours, but luckily we had guidance and inspiration from Cruzhacks' mentors."
+          attendee="4TH YEAR UNDERGRADUATE"
+          school="UC SANTA CRUZ"
+        />
+        <span className="retrospective__flexBreak" />
+        <img className="retrospective__image-2" src={retroImage2} />
+        <span className="retrospective__flexBreak" />
+        <img className="retrospective__image-3" src={retroImage3} />
+        <span className="retrospective__flexBreak" />
+        <div className="retrospective__secondQuote">
+          <QuoteView
+            quote="I'm happy to say that I am officially committing to pursue 
           my first long-term personal project thanks to all of the
           encouragement and positive feedback we got at the event!"
-          attendee="2ND YEAR UNDERGRADUATE"
-          school="UC IRVINE"
-        />
+            attendee="2ND YEAR UNDERGRADUATE"
+            school="UC IRVINE"
+          />
+        </div>
+        <img className="retrospective__image-4" src={retroImage4} />
       </div>
     </>
   );
