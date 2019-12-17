48584ec77c9afa3681338d770af75fae9d04aa17
Isai Lopez <ilopezro@ucsc.edu>
2019-12-16 16:45:21 -0800

    added folder for live webpage to live in

---
+++ src/components/live/live.view.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LiveView: React.FC = () => {
+    return(
+        <div>
+            hello from live view
+        </div>
+    );
+};
+
+export default LiveView;
