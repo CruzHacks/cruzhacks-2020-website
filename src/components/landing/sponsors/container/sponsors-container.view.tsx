1c196c9b5ce49a8b0e69162325254c30b81e3a17
mocklee <emockler@ucsc.edu>
2019-12-25 22:17:23 -0800

    included new sponsor logos

--- src/components/landing/sponsors/container/sponsors-container.view.tsx
+++ src/components/landing/sponsors/container/sponsors-container.view.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import logos from '../../../../assets/images/sponsors/sponsors.svg';
-
 interface sponsors {
   [index: string]: {
-    logo: HTMLObjectElement;
+    logo: string;
     URL: string;
     tier: string;
+    name: string;
   }[];
 }
 
@@ -14,13 +13,41 @@ const SponsorsContainer: React.FC<sponsors> = sponsors => {
   return (
     <div className="sponsors__boxContainer">
       <span className="sponsors__title">Thank You Sponsors</span>
-      <span className="sponsors__divider"></span>
-      <span className="sponsors__logosContainer"></span>
-      <img
-        alt="Amazon, Microsoft, Poly"
-        src={logos}
-        className="sponsors__logos"
-      ></img>
+      <span className="sponsors__divider" />
+      <div className="sponsors__logosContainer">
+        <div className="sponsors__logos-tera">
+          {sponsors.logos.map(
+            sponsor =>
+              sponsor.tier === 'tera' && (
+                <img src={sponsor.logo} alt="sponsor.name" />
+              )
+          )}
+        </div>
+        <div className="sponsors__logos-giga">
+          {sponsors.logos.map(
+            sponsor =>
+              sponsor.tier === 'giga' && (
+                <img src={sponsor.logo} alt="sponsor.name" />
+              )
+          )}
+        </div>
+        <div className="sponsors__logos-kilo">
+          {sponsors.logos.map(
+            sponsor =>
+              sponsor.tier === 'kilo' && (
+                <img src={sponsor.logo} alt="sponsor.name" />
+              )
+          )}
+        </div>
+        <div className="sponsors__logos-mega">
+          {sponsors.logos.map(
+            sponsor =>
+              sponsor.tier === 'mega' && (
+                <img src={sponsor.logo} alt="sponsor.name" />
+              )
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/landing/sponsors/sponsors.view.tsx
+++ src/components/landing/sponsors/sponsors.view.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import SponsorsContainer from './container/sponsors-container.view';
 
-const SponsorsView: React.FC = () => {
-    return(
-        <div className="sponsors__container">
-            <SponsorsContainer/>
-        </div>
-    )
-}
+const logos = [
+  {
+    tier: 'tera',
+    logo: '',
+    URL: '',
+    name: '',
+  },
+];
 
-export default SponsorsView
+const SponsorsView: React.FC = () => {
+  return (
+    <div className="sponsors__container">
+      <SponsorsContainer logos={logos} />
+    </div>
+  );
+};
+
+export default SponsorsView;
