428a3d933d1bae4cec8ac42b4f5e54407d2ac0c9
mocklee <emockler@ucsc.edu>
2019-12-25 21:36:48 -0800

    shortened FAQ answer width

--- src/components/landing/sponsors/container/sponsors-container.view.tsx
+++ src/components/landing/sponsors/container/sponsors-container.view.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 import logos from '../../../../assets/images/sponsors/sponsors.svg';
 
-const SponsorsContainer: React.FC = () => {
+interface sponsors {
+  [index: string]: {
+    logo: HTMLObjectElement;
+    URL: string;
+    tier: string;
+  }[];
+}
+
+const SponsorsContainer: React.FC<sponsors> = sponsors => {
   return (
     <div className="sponsors__boxContainer">
       <span className="sponsors__title">Thank You Sponsors</span>
