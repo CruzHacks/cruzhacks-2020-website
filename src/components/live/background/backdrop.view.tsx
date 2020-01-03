1adfc79cc74e0eeb7cf432686ac5209d618b4f4b
Isai Lopez <ilopezro@ucsc.edu>
2020-01-03 00:28:46 -0800

    added backdrop

---
+++ src/components/live/background/backdrop.view.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import topSVG from '../../../assets/live/topSVG.svg';
+
+const Backdrop: React.FC = () => {
+    return(
+        <div className="liveBackground__container">
+            <div className="liveBackground__backdropImage">
+                <img src={topSVG} alt="Top SVG" className="background__image"/>
+            </div>
+        </div>
+    );
+};
+
+export default Backdrop;
---
+++ src/components/live/homepage/homepage.view.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Navbar from '../navbar/navbar.view'
+import Backdrop from '../background/backdrop.view'
+
+const LiveHomepage: React.FC = () => {
+    return (
+        <div className="live__homepage-container">
+            <Backdrop />
+            <Navbar />
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+            <p>this is a sample p to test sticky</p>
+        </div>
+    );
+};
+
+export default LiveHomepage; 
