922f477b599b1838784643f13c6629e399c16fb7
Isai Lopez <ilopezro@ucsc.edu>
2020-01-05 18:05:12 -0800

    changed background div name

--- src/components/live/background/backdrop.view.tsx
+++ src/components/live/background/backdrop.view.tsx
@@ -5,7 +5,7 @@ const Backdrop: React.FC = () => {
     return(
         <div className="liveBackground__container">
             <div className="liveBackground__backdropImage">
-                <img src={topSVG} alt="Top SVG" className="background__image"/>
+                <img src={topSVG} alt="Top SVG" className="liveBackground__image"/>
             </div>
         </div>
     );
