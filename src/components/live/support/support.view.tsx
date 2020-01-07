3314380b1fe7411bf510349d86342ef49887ff79
Isai Lopez <ilopezro@ucsc.edu>
2020-01-07 14:05:35 -0800

    added support view page

--- src/components/live/support/support.view.tsx
+++ src/components/live/support/support.view.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from '../navbar/navbar.view'
 import Backdrop from '../background/backdrop.view'
+import SlackCardView from './slack/slack.view';
 
 const SupportView: React.FC = () => {
     return (
         <div className="support__container">
             <Backdrop />
             <Navbar />
-            Hello Support View
+            <div className="support__title">
+                Support and FAQ
+            </div>
+            <SlackCardView />
         </div>
     );
 };
