2ebeec991e79f78d0458239bbe39589680b78055
Isai Lopez <ilopezro@ucsc.edu>
2020-01-07 14:13:02 -0800

    added mlh code of conduct card

---
+++ src/components/live/support/mlh/mlhcoc.view.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MLHCodeOfConductCardView: React.FC = () => {
+    return(
+        <div className="mlhcoc-support__container">
+            <div className="mlhcoc-support__card">
+                <div className="mlhcoc-support__title">
+                    MLH Code of Conduct
+                </div>
+                <span className="mlhcoc-support__description">
+                    Cruzhacks values the importance of a safe and all-inclusive space. 
+                    We welcome students from all backgrounds. All attendees must adhere to our Code of Conduct.
+                </span>
+                <a className="mlhcoc-support__link" href="https://www.google.com" target="_blank" rel="noopener noreferrer">View Code Of Conduct</a>
+            </div>
+        </div>
+    );
+};
+
+export default MLHCodeOfConductCardView; 
