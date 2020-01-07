fcc7ff2dcb1f235b83466c7e8a78fcad993af2a9
Isai Lopez <ilopezro@ucsc.edu>
2020-01-07 13:01:40 -0800

    added devpost card to hackpack file

--- src/components/live/hackPack/hackPack.view.tsx
+++ src/components/live/hackPack/hackPack.view.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Navbar from '../navbar/navbar.view';
 import Backdrop from '../background/backdrop.view';
 import HackerPacketCardView from './hackerPacket/hackerpacket.view';
-import CrowdForgeCardView from './crowdforge/crowdforge.view'
+import CrowdForgeCardView from './crowdforge/crowdforge.view';
+import DevpostCardView from './devpost/devpost.view';
 
 const HackPackView: React.FC = () => {
     return (
@@ -12,6 +13,7 @@ const HackPackView: React.FC = () => {
             <span className="hackpack__title">Everything you need here.</span>
             <HackerPacketCardView />
             <CrowdForgeCardView />
+            <DevpostCardView />
         </div>
     );
 };
