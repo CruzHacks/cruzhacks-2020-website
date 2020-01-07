e688adc16a74e22ac5bca1197e3482303b889ca1
Isai Lopez <ilopezro@ucsc.edu>
2020-01-07 12:28:39 -0800

    hacker packet card finished

--- src/components/live/hackPack/hackPack.view.tsx
+++ src/components/live/hackPack/hackPack.view.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from '../navbar/navbar.view'
 import Backdrop from '../background/backdrop.view'
+import HackerPacketCardView from './hackerPacket/hackerpacket.view'
 
 const HackPackView: React.FC = () => {
     return (
         <div className="hackpack__container">
             <Backdrop />
             <Navbar />
-            Hello Hack Pack
+            <span className="hackpack__title">Everything you need here.</span>
+            <HackerPacketCardView/>
         </div>
     );
 };
---
+++ src/components/live/hackPack/hackerPacket/hackerpacket.view.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const HackerPacketCardView: React.FC = () => {
+    return(
+        <div className="hacker-packet__container">
+            <div className="hacker-packet__card">
+                <span className="hacker-packet__slug-on">
+                    Ready to get your slug on?
+                </span>
+                <div className="hacker-packet__title">
+                    Hacker Packet
+                </div>
+                <span className="hacker-packet__info">
+                    You can find everything you need to know in our hacker packet PDF. 
+                    It covers what you need to bring, travel info, workshop info, and more.
+                </span>
+                <a className="hacker-packet__packet" href="https://www.google.com" target="_blank" rel="noopener noreferrer">View Hacker Packet</a>
+            </div>
+        </div>
+    );
+};
+
+export default HackerPacketCardView;
