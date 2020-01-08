ce492e06c8acef3880a31e01560604b46c0c1213
Isai Lopez <ilopezro@ucsc.edu>
2020-01-08 14:17:29 -0800

    added faq to support file

--- src/components/live/support/support.view.tsx
+++ src/components/live/support/support.view.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
-import Navbar from '../navbar/navbar.view'
-import Backdrop from '../background/backdrop.view'
+import Navbar from '../navbar/navbar.view';
+import Backdrop from '../background/backdrop.view';
 import SlackCardView from './slack/slack.view';
+import MLHCodeOfConductCardView from './mlh/mlhcoc.view';
+import EmergencyContactCardView from './emergency/emergency.view';
+import FAQCardView from './faq/faq.view';
+
+const FAQQandA = [
+    {
+        question: "Are we allowed to build on past projects?",
+        answer: "Ask team about this."
+    }, 
+    {
+        question: "How many people can be on my team?", 
+        answer: "You and 4 others."
+    }, 
+    {
+        question: "What are the rules?", 
+        answer: "redirect them to rules?"
+    }, 
+    {
+        question: "What if I don't know how to code.", 
+        answer: "Beep boop beep. There is no need!"
+    }, 
+    {
+        question: "How can I get a mentor?", 
+        answer: "Good question. Answer this later"
+    }, 
+    {
+        question: "I don't see my question here.",
+        answer: "Please message us on Slack for other question!"
+    }
+]
 
 const SupportView: React.FC = () => {
     return (
@@ -12,6 +42,9 @@ const SupportView: React.FC = () => {
                 Support and FAQ
             </div>
             <SlackCardView />
+            <FAQCardView qAndA={FAQQandA}/>
+            <MLHCodeOfConductCardView />
+            <EmergencyContactCardView />
         </div>
     );
 };
