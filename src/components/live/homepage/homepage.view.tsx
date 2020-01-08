bc07f6826418b93cd6cba1cad5ae723da39e4857
mocklee <emockler@ucsc.edu>
2020-01-07 23:23:14 -0800

    split cards into views

--- src/components/live/homepage/homepage.view.tsx
+++ src/components/live/homepage/homepage.view.tsx
@@ -1,37 +1,23 @@
 import React from 'react';
-import Navbar from '../navbar/navbar.view'
-import Backdrop from '../background/backdrop.view'
-import QuestionView from './question/question.view'
+import Navbar from '../navbar/navbar.view';
+import Backdrop from '../background/backdrop.view';
+import QuestionView from './question/question.view';
+import UpdatesView from './updates/updates.view';
+import ScheduleView from './schedule/schedule.view';
+import WorkshopsView from './workshops/workshops.view';
 
 const LiveHomepage: React.FC = () => {
-    return (
-        <div className="homepage__container">
-            <Backdrop />
-            <Navbar />
-            <div className="homepage__title">
-                CruzHacks 2020 Live
-            </div>
-            <div className="homepage__updates-card-container">
-                <div className="homepage__updates-card">
-                    <span className="homepage__live-update-title">
-                        <span className="homepage__live-update-blinker"></span>
-                        Live Updates
-                    </span>
-                </div>
-            </div>
-            <div className="homepage__gen-schedule-card-container">
-                <div className="homepage__gen-schedule-card">
-                    <span className="homepage__general-schedule-title">General Schedule</span>
-                </div>
-            </div>
-            <div className="homepage__workshops-card-container">
-                <div className="homepage__workshops-card">
-                    <span className="homepage__workshops-title">Workshops</span>
-                </div>
-            </div>
-            <QuestionView />
-        </div>
-    );
+  return (
+    <div className="homepage__container">
+      <Backdrop />
+      <Navbar />
+      <span className="homepage__title">CruzHacks 2020 Live</span>
+      <UpdatesView />
+      <ScheduleView />
+      <WorkshopsView />
+      <QuestionView />
+    </div>
+  );
 };
 
-export default LiveHomepage; 
+export default LiveHomepage;
---
+++ src/components/live/homepage/schedule/schedule.view.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ScheduleView: React.FC = () => {
+  return (
+    <>
+      <div className="homepage__gen-schedule-card-container">
+        <div className="homepage__gen-schedule-card">
+          <span className="homepage__general-schedule-title">
+            General Schedule
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ScheduleView;
