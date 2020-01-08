49da08a93d90de46e42082e30fa77df7e7148438
mocklee <emockler@ucsc.edu>
2020-01-07 22:51:09 -0800

    navbar responsive on desktop

--- src/components/live/homepage/question/question.view.tsx
+++ src/components/live/homepage/question/question.view.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
-import question from '../../../../assets/live/homepage/question.svg'
+import question from '../../../../assets/live/homepage/question.svg';
 
 const QuestionView: React.FC = () => {
-    return(
-        <div className="questions__card-container">
-                <div className="questions__card">
-                    <span className="questions__title">Have a Question?</span>
-                    <img src={question} className="questions__image" alt="question"></img>
-                    <span className="questions__paragraph">
-                        Our organizing team is available throughout the whole event. 
-                        If there’s anything you need please contact us through slack or find an organizer in person.
-                    </span>
-                    <a className="questions__join-slack" href="https://www.google.com" target="_blank" rel="noopener noreferrer">Join Our Slack</a>
-                    <a className="questions__goto-hackpack" href="/live/hackpack" rel="noopener noreferrer">Go To Hack Pack</a>
-                </div>
-            </div>
-    );
+  return (
+    <div className="questions__card-container">
+      <div className="questions__card">
+        <span className="questions__title">Have a Question?</span>
+        <img src={question} className="questions__image" alt="question"></img>
+        <span className="questions__paragraph">
+          Our organizing team is available throughout the whole event. If
+          there’s anything you need please contact us through slack or find an
+          organizer in person.
+        </span>
+        <div className="questions__button-container">
+          <a
+            className="questions__join-slack"
+            href="https://www.google.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Join Our Slack
+          </a>
+          <a
+            className="questions__goto-hackpack"
+            href="/live/hackpack"
+            rel="noopener noreferrer"
+          >
+            Go To Hack Pack
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QuestionView;
