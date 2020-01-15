020e95a28e90775c097b380e769d0119dc5d02c5
Isai Lopez <ilopezro@ucsc.edu>
2020-01-14 22:38:31 -0800

    added schedules for mapping

---
+++ src/components/live/homepage/workshopfood/schedules/schedules.tsx
@@ -0,0 +1,201 @@
+export const DayOneWorkshops = [
+    {
+        title: "Intro to CNNs (Convolutional Neural Networks)",
+        location: "Stevenson 151",
+        time: "10:00pm-10:45pm",
+        difficulty: "Advanced"
+    }, 
+    {
+        title: "Intro to Raspberry Pi",
+        location: "Stevenson 152",
+        time: "10:00pm-10:45pm",
+        difficulty: "Intermediate"
+    },
+    {
+        title: "Intro to Git/Github/Bash",
+        location: "Stevenson 152",
+        time: "10:45pm-11:30pm",
+        difficulty: "Beginner"
+    }
+]
+
+export const DayTwoWorkshops = [
+    {
+        title: "Intro to Web Dev",
+        location: "Stevenson 151",
+        time: "10:30am-11:15am",
+        difficulty: "Beginner"
+    }, 
+    {
+        title: "Intro to Python",
+        location: "Stevenson 152",
+        time: "10:30am-11:15am",
+        difficulty: "Beginner"
+    },
+    {
+        title: "Alexa",
+        location: "Stevenson 151",
+        time: "11:30am-12:30pm",
+        difficulty: "Intermediate"
+    },
+    {
+        title: "Intro to Android Development",
+        location: "Stevenson 152",
+        time: "11:30am-12:30pm",
+        difficulty: "Intermediate"
+    },
+    {
+        title: "Pandas using Jupyter Notebooks",
+        location: "Stevenson 151",
+        time: "11:30am-12:30pm",
+        difficulty: "Intermediate"
+    },
+    {
+        title: "Building Data Applications with Looker",
+        location: "Stevenson 152",
+        time: "11:30am-12:30pm",
+        difficulty: "Intermediate"
+    },
+    {
+        title: "Resume/Cover Letter",
+        location: "Stevenson 151",
+        time: "1:30pm-2:15pm",
+        difficulty: "Beginner"
+    },
+    {
+        title: "React JS",
+        location: "Stevenson 152",
+        time: "1:30pm-2:15pm",
+        difficulty: "Intermediate"
+    }, 
+    {
+        title: "AWS",
+        location: "Stevenson 151",
+        time: "2:30pm-3:30pm",
+        difficulty: "Advanced"
+    },
+    {
+        title: "Super Smash Bros",
+        location: "Stevenson 150",
+        time: "2:30pm-6:15pm",
+        difficulty: "Any"
+    },
+    {
+        title: "Technical Interview",
+        location: "Stevenson 151",
+        time: "3:30pm-4:15pm",
+        difficulty: "Advanced"
+    },
+    {
+        title: "Google Cloud Platform: Developing Products",
+        location: "Stevenson 151",
+        time: "4:30pm-5:30pm",
+        difficulty: "Advanced"
+    },
+    {
+        title: "Intro to 3D Printing",
+        location: "Stevenson 151",
+        time: "5:30pm-6:15pm",
+        difficulty: "Beginner"
+    },
+    {
+        title: "Docker",
+        location: "Stevenson 151",
+        time: "6:30pm-7:15pm",
+        difficulty: "Intermediate"
+    },
+    {
+        title: "Intro to Web Scraping",
+        location: "Stevenson 152",
+        time: "6:30pm-7:15pm",
+        difficulty: "Advanced"
+    }
+]
+
+export const DayOneFood = [
+    {
+        typeAndTime: "Dinner - 5:00pm", 
+        food: "Dominos", 
+        description: [
+            "Pepperoni", "Cheese", "Green Bell Pepper"
+        ]
+    }, 
+    {
+        food: "Wingstop", 
+        description: [
+            "Cajun", "Louisiana Rub", "Garlic Parmesan", "Celery and Carrot Sticks"
+        ]
+    },
+    {
+        typeAndTime: "Midnight Snack - 12:00 AM",
+        food: "Pacific Cookie Company",
+        description: [
+            "cookies", "for", "all"
+        ]
+    }
+]
+
+export const DayTwoFood = [
+    {
+        typeAndTime: "Breakfast - 8:00am", 
+        food: "The Bagelry", 
+        description: [
+            "Bagels", "Cream Cheese"
+        ]
+    },
+    {
+        typeAndTime: "Lunch - 1:00 PM",
+        food: "HOM's Korean Kitchen", 
+        description: [
+            "White Rice", "Salad", "BBQ Chicken", "Firecracker Pork", "Broccoli", "Carrots", "Cucumber", "Bean Sprouts"
+        ]
+    },
+    {
+        typeAndTime: "Dinner - 5:30PM",
+        food: "Panda Express",
+        description: [
+            "Orange Chicken and Fried Rice",
+            "Broccoli Beef and Chow Mein",
+            "Eggplant Tofu and Fried Rice"
+        ]
+    }, 
+    {
+        typeAndTime: "Late Nigh Snack - 12:00 am",
+        food: "Ferrel's Donuts",
+        description: [
+            "DONUTS!!"
+        ]
+    }
+]
+
+export const DayThreeFood = [
+    {
+        typeAndTime: "Breakfast - 8:00am", 
+        food: "The Bagelry",
+        description: [
+            "Bagels", "Cream Cheese"
+        ]
+    },
+    {
+        typeAndTime: "Lunch - 1:00pm", 
+        food: "Ike's Sandwiches", 
+        description: [
+            "Matt cain (Godfather Sauce, Provolone, Roast Beef, Salami, Turkey)", 
+            "Barry Bonds (Turkey, Bacon, Swiss)",
+            "Menage a Trois (Halal Chicken, Real Honey, Honey Mustard, BBQ, Pepper Jack, Swiss, Cheddar)",
+            "Sometimes I’m a Vegetarian ( Grilled Mushrooms, Marinated Artichoke Hearts, Pesto, Provolone)",
+            "Reading Rainbow (Avacado, Ike’s Red Pesto, Cheddar)"
+        ]
+    },
+    {
+        typeAndTime: "Judges's Lunch",
+        food: "Falafel of Santa Cruz",
+        description: [
+            "Falafels",
+            "Hummus",
+            "Chicken Shwarma", 
+            "Mediterranean Salad",
+            "Pita Bread"
+        ]
+    }
+]
