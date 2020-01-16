2b89bb39aad822aa73d2fa5cab7c98ce511b1aef
Isai Lopez <ilopezro@ucsc.edu>
2020-01-16 15:27:51 -0800

    added live updates

--- src/components/live/homepage/updates/updates.view.tsx
+++ src/components/live/homepage/updates/updates.view.tsx
@@ -1,6 +1,71 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useEffect} from 'react';
+import { getAnnoucements } from '../../../../account';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import { useInterval, getDateToString, getTimeToString } from './helperFunctions/functions';
+
+interface Post {
+  announcement: string;
+  datetime: string;
+}
 
 const UpdatesView: React.FC = () => {
+
+  var posts: any; 
+  const [postArray, setPostArray] = useState<Post[]>([]);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useInterval(function() {
+    getAnnoucements()
+      .then(response => {
+        posts = response;
+        setPostArray(posts.reverse());
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }, isMounted ? 5000 : null);
+
+  useEffect(() => {
+    setIsMounted(isMounted => !isMounted);
+    getAnnoucements()
+      .then(response => {
+        if(!isMounted){
+          posts = response; 
+          setPostArray(posts.reverse());
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      });
+
+    return () => {
+      setIsMounted(isMounted => !isMounted);
+    };
+  }, [])
+
+  const Updates = () => {
+    return(
+      <div className="updates-card__updates-container">
+        <PerfectScrollbar>
+          {
+            postArray.map((post, i) => (
+              <div className="updates-card__announcement" key={post.announcement}>
+                <span className="updates-card__post-time">{getTimeToString(post)}</span>
+                <span className="updates-card__post-info">{post.announcement}</span>
+                <span className="updates-card__post-date">{getDateToString(post)}</span>
+                {i === postArray.length-1 ? <div className="updates-card__end-line-break"></div>:<div className="updates-card__line-break"></div>}
+                
+              </div>
+            ))
+          }
+        </PerfectScrollbar>
+        
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="updates-card__container">
@@ -9,6 +74,7 @@ const UpdatesView: React.FC = () => {
             <span className="updates-card__blinker"></span>
             <span className="updates-card__title">Live Updates</span>
           </div>
+          {<Updates /> }
         </div>
       </div>
     </>
