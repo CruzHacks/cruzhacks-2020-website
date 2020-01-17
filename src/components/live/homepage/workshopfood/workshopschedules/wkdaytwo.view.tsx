ed33b76252509bad8cf2f56d2727cfef87522eb2
mocklee <emockler@ucsc.edu>
2020-01-17 11:34:37 -0800

    day 2 responsive

--- src/components/live/homepage/workshopfood/workshopschedules/wkdaytwo.view.tsx
+++ src/components/live/homepage/workshopfood/workshopschedules/wkdaytwo.view.tsx
@@ -4,35 +4,40 @@ import PerfectScrollbar from 'react-perfect-scrollbar';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 
 const WorkshopsDayTwoView: React.FC = () => {
-
-    const WorkshopView = () => {
-        return(
-            <div className="workshop-schedule-two__events-container">
-                <PerfectScrollbar>
-                    {DayTwoWorkshops.map(event => (
-                        <div className="workshop-schedule-two__events" key={event.title}>
-                        <span className="workshop-schedule-two__event-title">{event.title}</span>
-                        <span className="workshop-schedule-two__event-location">
-                            {event.location}
-                            <span className="workshop-schedule-two__event-time">{event.time}</span>
-                        </span>
-                        <span className="workshop-schedule-two__event-difficulty">{event.difficulty}</span>
-                    </div>
-                    ))}
-                </PerfectScrollbar>
+  const WorkshopView = () => {
+    return (
+      <div className="workshop-schedule-two__events-container">
+        <PerfectScrollbar>
+          {DayTwoWorkshops.map(event => (
+            <div className="workshop-schedule-two__events" key={event.title}>
+              <span className="workshop-schedule-two__event-time">
+                {event.time}
+              </span>
+              <span className="workshop-schedule-two__event-title">
+                {event.title}
+              </span>
+              <span className="workshop-schedule-two__event-location">
+                {event.location}
+              </span>
+              <span className="workshop-schedule-two__event-difficulty">
+                {event.difficulty}
+              </span>
             </div>
-        );
-    }
-
-    return(
-        <div className="workshop-schedule-two__container">
-            <span className="workshop-schedule-two__date">
-                Saturday, January 18th
-            </span>
-            <div className="workshop-schedule-two__line-break"></div>
-            <WorkshopView />
-        </div>
+          ))}
+        </PerfectScrollbar>
+      </div>
     );
+  };
+
+  return (
+    <div className="workshop-schedule-two__container">
+      <span className="workshop-schedule-two__date">
+        Saturday, January 18th
+      </span>
+      <div className="workshop-schedule-two__line-break"></div>
+      <WorkshopView />
+    </div>
+  );
 };
 
-export default WorkshopsDayTwoView; 
+export default WorkshopsDayTwoView;
