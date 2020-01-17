30a5a5af8166bd708e2cccb52b46b25ed4d4d4ed
mocklee <emockler@ucsc.edu>
2020-01-17 11:22:39 -0800

    partially fixed workshops day 1

--- src/components/live/homepage/workshopfood/workshopschedules/wkdayone.view.tsx
+++ src/components/live/homepage/workshopfood/workshopschedules/wkdayone.view.tsx
@@ -1,34 +1,37 @@
 import React from 'react';
-import { DayOneWorkshops } from '../schedules/schedules'
+import { DayOneWorkshops } from '../schedules/schedules';
 
 const WorkshopsDayOneView: React.FC = () => {
-    
-    const WorkshopsView = () => {
-        return(
-            <div className="workshop-schedule-one__events-container">
-                {DayOneWorkshops.map(event =>(
-                    <div className="workshop-schedule-one__events" key={event.title}>
-                        <span className="workshop-schedule-one__event-title">{event.title}</span>
-                        <span className="workshop-schedule-one__event-location">
-                            {event.location}
-                                <span className="workshop-schedule-one__event-time">{event.time}</span>
-                        </span>
-                        <span className="workshop-schedule-one__event-difficulty">{event.difficulty}</span>
-                    </div>
-                ))}
-            </div>
-        );
-    };
-    
-    return(
-        <div className="workshop-schedule-one__container">
-            <span className="workshop-schedule-one__date">
-                Friday, January 17th
+  const WorkshopsView = () => {
+    return (
+      <div className="workshop-schedule-one__events-container">
+        {DayOneWorkshops.map(event => (
+          <div className="workshop-schedule-one__events" key={event.title}>
+            <span className="workshop-schedule-one__event-time">
+              {event.time}
             </span>
-            <div className="workshop-schedule-one__line-break"></div>
-            <WorkshopsView/>
-        </div>
+            <span className="workshop-schedule-one__event-title">
+              {event.title}
+            </span>
+            <span className="workshop-schedule-one__event-location">
+              {event.location}
+            </span>
+            <span className="workshop-schedule-one__event-difficulty">
+              {event.difficulty}
+            </span>
+          </div>
+        ))}
+      </div>
     );
+  };
+
+  return (
+    <div className="workshop-schedule-one__container">
+      <span className="workshop-schedule-one__date">Friday, January 17th</span>
+      <div className="workshop-schedule-one__line-break"></div>
+      <WorkshopsView />
+    </div>
+  );
 };
 
 export default WorkshopsDayOneView;
