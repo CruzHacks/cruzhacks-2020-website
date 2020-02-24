971ae3763b570dd17bd694145bb3334cc7abe9f3
Isai Lopez <ilopezro@ucsc.edu>
2020-02-23 21:00:14 -0800

    removed unnecesary components

--- src/components/postLanding/hero/postLandingHero.view.tsx
+++ src/components/postLanding/hero/postLandingHero.view.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import RightBackdrop from '../../../assets/postLanding/right-backdrop.svg';
 import BackdropHeroAllPillars from '../../landing/background/backdrop/landing/hero/backdrop-hero-all-pillars.view';
 import BackdropHeroRightSideView from '../../landing/background/backdrop/landing/hero/backdrop-hero-rightside.view';
 import TitleAreaView from './titleArea/titleArea.view';
-import HeroHeaderView from '../../landing/hero/header/hero-header.view';
 import HeroHamburgerView from '../../landing/hero/header/hero-hamburger.view';
-import HeroNavbarView from '../../landing/hero/header/navbar/hero-navbar.view';
 import NavbarView from './navbar/navbar.view';
 
 const PostLandingHeroView: React.FC = () => {
