afa47122f94074dd0282c6b0e0940b7604c002d2
Isai Lopez <ilopezro@ucsc.edu>
2020-02-24 18:19:09 -0800

    added init about component

---
+++ src/components/postLanding/about/about.view.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const PostLandingAboutView: React.FC = () => {
+    return(
+        <>
+        <div className="post-landing-about__container">
+            <span className="post-landing-about__title">
+                We're Not Just A Hackathon
+            </span>
+            <span className="post-landing-about__text">
+                From January 17 to January 19, 2020, UCSC’s Stevenson Event Center was transformed into a bustling playground for our diverse group of over 600 hackers to turn their visions for social change into tangible projects. The 36 hours of hacking were as hectic as they were productive. As hackers downed bottle after bottle of Yerba Mate and Soylent, mentors and your representatives were available to advise teams on their project ideas and work out any challenges they encountered along the way
+            </span>
+        </div>
+        </>
+    );
+}
+
+export default PostLandingAboutView;
