f42a25611d89e449ee35cadf752831849065c09f
Isai Lopez <ilopezro@ucsc.edu>
2020-02-24 18:19:21 -0800

    added about component to base scss and also to vie w

--- src/components/postLanding/postLanding.view.tsx
+++ src/components/postLanding/postLanding.view.tsx
@@ -3,12 +3,14 @@ import PostLandingHeroView from './hero/postLandingHero.view';
 import PostLandingShareView from './share/postLandingShare.view';
 import PostLandingCTAview from './cta/postLandingCTA.view';
 import SponsorsView from '../landing/sponsors/sponsors.view';
+import PostLandingAboutView from './about/about.view';
 
 const PostLandingView: React.FC = () => {
   return (
     <>
       <div className="postLanding__container">
         <PostLandingHeroView />
+        <PostLandingAboutView/>
         <SponsorsView />
         <PostLandingCTAview />
         <PostLandingShareView />
