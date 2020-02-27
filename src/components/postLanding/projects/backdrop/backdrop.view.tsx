146c19fccd1c50a19539816df43936d5a82bef0e
Isai Lopez <ilopezro@ucsc.edu>
2020-02-27 15:31:59 -0800

    added more reponsivness to projects sectoin

--- src/components/postLanding/projects/backdrop/backdrop.view.tsx
+++ src/components/postLanding/projects/backdrop/backdrop.view.tsx
@@ -3,16 +3,18 @@ import React from 'react';
 const BackdropView: React.FC = () => {
     return(
         <>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1525" height="566" className="post-landing-projects__backdrop">
-        <defs>
-            <image width="1152" height="365" id="backdrop-1" href="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTE1MiIgaGVpZ2h0PSIzNjUiIHZpZXdCb3g9IjAgMCAxMTUyIDM2NSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTEyOTUgMTA0LjU2N0wxMDUwLjM3IDcuMzExM0MxMDI1Ljg2IC0yLjQzMzE0IDk5Ni44OTYgLTIuNDM2NzcgOTcyLjM4NyA3LjMwMDM5TDc5Ni4xNDEgNzcuMzEyQzc2My4zNjkgOTAuMzMgNzI0LjY0NyA5MC4zNDgyIDY5MS44NTcgNzcuMzU1Nkw2MDMuMjgxIDQyLjI2NjJDNTg2LjcxIDM1LjcwMDggNTY3LjEzIDM1Ljc0NDQgNTUwLjYwNSA0Mi4zODYyQzU1MC42MDUgNDIuMzg2MiAzNTEgOTYuMDMxOCAyNjEgNDIuMjY1OUMxNzEgLTExLjUgLTEgNzcuMzEyIC0xIDc3LjMxMlYzNjMuMTgyTDEyOTUgMzY1VjEwNC41NjdaIiBmaWxsPSJ1cmwoI3BhaW50MF9saW5lYXIpIi8+CjxkZWZzPgo8bGluZWFyR3JhZGllbnQgaWQ9InBhaW50MF9saW5lYXIiIHgxPSI4MDEuMjA1IiB5MT0iNTYuNjQ2MyIgeDI9IjgwNC42NjgiIHkyPSI4MjguNDg3IiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+CjxzdG9wIHN0b3AtY29sb3I9IiNEN0M5RkMiLz4KPHN0b3Agb2Zmc2V0PSIwLjM0ODk1OCIgc3RvcC1jb2xvcj0id2hpdGUiLz4KPC9saW5lYXJHcmFkaWVudD4KPC9kZWZzPgo8L3N2Zz4K"/>
-            <image width="1149" height="429" id="backdrop-2" href="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTE0OSIgaGVpZ2h0PSI0MjkiIHZpZXdCb3g9IjAgMCAxMTQ5IDQyOSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTS04IDEyMi45MDFMMjc5LjM0NyA4LjU5M0MzMDguMTM2IC0yLjg2MDA2IDM0Mi4xNTkgLTIuODY0MzMgMzcwLjk0NyA4LjU4MDE3TDU3Ny45NyA5MC44Njc3QzYxNi40NjQgMTA2LjE2OCA2NjEuOTQ4IDEwNi4xOSA3MDAuNDYzIDkwLjkxOUw4MDQuNTA2IDQ5LjY3NjlDODIzLjk3MSA0MS45NjAzIDg0Ni45NyA0Mi4wMTE2IDg2Ni4zOCA0OS44MThMMTA0OC4xMyAxMjIuOTAxTDExNDkgMTYxLjgwNVY0MjYuODYyTC04IDQyOVYxMjIuOTAxWiIgZmlsbD0idXJsKCNwYWludDBfbGluZWFyKSIgZmlsbC1vcGFjaXR5PSIwLjciLz4KPGRlZnM+CjxsaW5lYXJHcmFkaWVudCBpZD0icGFpbnQwX2xpbmVhciIgeDE9IjU3Mi4wMjEiIHkxPSI2Ni41Nzg4IiB4Mj0iNTY3Ljk0OCIgeTI9Ijk3My43NTYiIGdyYWRpZW50VW5pdHM9InVzZXJTcGFjZU9uVXNlIj4KPHN0b3Agc3RvcC1jb2xvcj0iI0I3RjdGMyIvPgo8c3RvcCBvZmZzZXQ9IjAuMzQ4OTU4IiBzdG9wLWNvbG9yPSJ3aGl0ZSIvPgo8L2xpbmVhckdyYWRpZW50Pgo8L2RlZnM+Cjwvc3ZnPgo="/>
-        </defs>
-        <style>
-        </style>
-        <use id="Vector" href="#backdrop-1" transform="matrix(1.333 0 0 1.332 0 16)"/>
-        <use id="Vector" href="#backdrop-2" transform="scale(1.333 1.332)"/>
-        </svg>
+        <div className="post-landing-projects__backdrop">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1525" height="566">
+                <defs>
+                    <image width="1152" height="365" id="backdrop-1" href="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTE1MiIgaGVpZ2h0PSIzNjUiIHZpZXdCb3g9IjAgMCAxMTUyIDM2NSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTEyOTUgMTA0LjU2N0wxMDUwLjM3IDcuMzExM0MxMDI1Ljg2IC0yLjQzMzE0IDk5Ni44OTYgLTIuNDM2NzcgOTcyLjM4NyA3LjMwMDM5TDc5Ni4xNDEgNzcuMzEyQzc2My4zNjkgOTAuMzMgNzI0LjY0NyA5MC4zNDgyIDY5MS44NTcgNzcuMzU1Nkw2MDMuMjgxIDQyLjI2NjJDNTg2LjcxIDM1LjcwMDggNTY3LjEzIDM1Ljc0NDQgNTUwLjYwNSA0Mi4zODYyQzU1MC42MDUgNDIuMzg2MiAzNTEgOTYuMDMxOCAyNjEgNDIuMjY1OUMxNzEgLTExLjUgLTEgNzcuMzEyIC0xIDc3LjMxMlYzNjMuMTgyTDEyOTUgMzY1VjEwNC41NjdaIiBmaWxsPSJ1cmwoI3BhaW50MF9saW5lYXIpIi8+CjxkZWZzPgo8bGluZWFyR3JhZGllbnQgaWQ9InBhaW50MF9saW5lYXIiIHgxPSI4MDEuMjA1IiB5MT0iNTYuNjQ2MyIgeDI9IjgwNC42NjgiIHkyPSI4MjguNDg3IiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+CjxzdG9wIHN0b3AtY29sb3I9IiNEN0M5RkMiLz4KPHN0b3Agb2Zmc2V0PSIwLjM0ODk1OCIgc3RvcC1jb2xvcj0id2hpdGUiLz4KPC9saW5lYXJHcmFkaWVudD4KPC9kZWZzPgo8L3N2Zz4K"/>
+                    <image width="1149" height="429" id="backdrop-2" href="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTE0OSIgaGVpZ2h0PSI0MjkiIHZpZXdCb3g9IjAgMCAxMTQ5IDQyOSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTS04IDEyMi45MDFMMjc5LjM0NyA4LjU5M0MzMDguMTM2IC0yLjg2MDA2IDM0Mi4xNTkgLTIuODY0MzMgMzcwLjk0NyA4LjU4MDE3TDU3Ny45NyA5MC44Njc3QzYxNi40NjQgMTA2LjE2OCA2NjEuOTQ4IDEwNi4xOSA3MDAuNDYzIDkwLjkxOUw4MDQuNTA2IDQ5LjY3NjlDODIzLjk3MSA0MS45NjAzIDg0Ni45NyA0Mi4wMTE2IDg2Ni4zOCA0OS44MThMMTA0OC4xMyAxMjIuOTAxTDExNDkgMTYxLjgwNVY0MjYuODYyTC04IDQyOVYxMjIuOTAxWiIgZmlsbD0idXJsKCNwYWludDBfbGluZWFyKSIgZmlsbC1vcGFjaXR5PSIwLjciLz4KPGRlZnM+CjxsaW5lYXJHcmFkaWVudCBpZD0icGFpbnQwX2xpbmVhciIgeDE9IjU3Mi4wMjEiIHkxPSI2Ni41Nzg4IiB4Mj0iNTY3Ljk0OCIgeTI9Ijk3My43NTYiIGdyYWRpZW50VW5pdHM9InVzZXJTcGFjZU9uVXNlIj4KPHN0b3Agc3RvcC1jb2xvcj0iI0I3RjdGMyIvPgo8c3RvcCBvZmZzZXQ9IjAuMzQ4OTU4IiBzdG9wLWNvbG9yPSJ3aGl0ZSIvPgo8L2xpbmVhckdyYWRpZW50Pgo8L2RlZnM+Cjwvc3ZnPgo="/>
+                </defs>
+                <style>
+                </style>
+                <use id="Vector" href="#backdrop-1" transform="matrix(1.333 0 0 1.332 0 16)"/>
+                <use id="Vector" href="#backdrop-2" transform="scale(1.333 1.332)"/>
+            </svg>
+        </div>
         </>
     );
 }
