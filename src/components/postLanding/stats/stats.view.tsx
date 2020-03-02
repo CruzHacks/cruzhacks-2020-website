f40e4abaa4b46e5087ceda9a7337feb61b5bab6f
mocklee <emockler@ucsc.edu>
2020-03-02 14:50:38 -0800

    fixed anchor hierarchy in dom

--- src/components/postLanding/stats/stats.view.tsx
+++ src/components/postLanding/stats/stats.view.tsx
@@ -3,34 +3,34 @@ import PostLandingStatsTreeView from './tree/tree.view';
 import PostLandingStatsBoxView from './box/box.view';
 
 let stats = [
-    {
-      category: 'attendees',
-      stat: '500+',
-    },
-    {
-      category: 'first time hackers',
-      stat: '47%',
-    },
-    {
-      category: 'projects',
-      stat: '60+',
-    },
-    {
-      category: '$ in prizes',
-      stat: '22,000+',
-    },
-  ];
+  {
+    category: 'attendees',
+    stat: '500+',
+  },
+  {
+    category: 'first time hackers',
+    stat: '47%',
+  },
+  {
+    category: 'projects',
+    stat: '60+',
+  },
+  {
+    category: '$ in prizes',
+    stat: '22,000+',
+  },
+];
 
 const PostLandingStatsView: React.FC = () => {
-    return(
-        <>
-            <div className="post-landing-stats__container">
-                <span className="post-landing-stats__title">2020 Success Stats</span>
-                <PostLandingStatsTreeView />
-                <PostLandingStatsBoxView stats={stats} />
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="post-landing-stats__container">
+        <span className="post-landing-stats__title">Milestones of 2020</span>
+        <PostLandingStatsTreeView />
+        <PostLandingStatsBoxView stats={stats} />
+      </div>
+    </>
+  );
+};
 
 export default PostLandingStatsView;
