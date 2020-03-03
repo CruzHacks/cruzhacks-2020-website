dc39f4bdd4b65c96b48a25c48b4f6798bddafdc9
mocklee <emockler@ucsc.edu>
2020-03-02 17:49:41 -0800

    fixed projects button centering

--- src/components/postLanding/projects/backdrop/backdrop.view.tsx
+++ src/components/postLanding/projects/backdrop/backdrop.view.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { ReactComponent as GreenWaves } from './greenWaves.svg';
-import { ReactComponent as PurpleWaves } from './purpleWaves.svg';
 
 const BackdropView: React.FC = () => {
   return (
     <>
       <div className="post-landing-projects__backdrop">
-        <PurpleWaves />
+        <div className="post-landing-projects__backdrop--purpleWaves" />
         <GreenWaves />
       </div>
     </>
