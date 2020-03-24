ee5eb2673c0a54743727274325713521fbae0c05
mocklee <emockler@ucsc.edu>
2020-03-23 21:06:44 -0700

    winners component responsive to mobile

--- src/components/postLanding/winners/winners.view.tsx
+++ src/components/postLanding/winners/winners.view.tsx
@@ -2,96 +2,99 @@ import React from 'react';
 import WinnersBoxView from './box/box.view';
 
 let winners = [
-    {
-        projectTitle: "TailoredChef",
-        projectDescription: "An app that creates tailored-made meal recommendations for young professionals to achieve nutritional goals, develop culinary mastery, and live healthier and more sustainable lives.",
-        category: "Health Hacks",
-        path: "tailoredchef",
-        listOfWinners: [
-            {
-                name: "Timothy Nguyen",
-                linkedin: "https://www.linkedin.com/in/timothy-nguyen-cs/"
-            },
-            {
-                name: "Austin Ha",
-                linkedin: "https://www.linkedin.com/in/austin-ha-2885b016b/"
-            },
-            {
-                name: "Zachary Boyd",
-                linkedin: "https://www.linkedin.com/in/zackboydt/"
-            },
-            {
-                name: "Leena Elzeiny",
-                linkedin: "https://www.linkedin.com/in/leena-elzeiny-a706a4143/"
-            },
-            {
-                name: "Noel Gomez",
-                linkedin: "https://www.linkedin.com/in/noel-gomez/"
-            }
-        ]
-    },
-    {
-        projectTitle: "Transparency Project", 
-        projectDescription: "The Transparency Project facilitates the understanding of our political process through interactive data visualization. By aggregating data from social media platforms, search engines, and ad...",
-        category: "Justice Hacks",
-        path: "transparencyproject",
-        listOfWinners: [
-            {
-                name: "Oasys Okubu",
-                linkedin: "https://www.linkedin.com/in/oasysokubo/"
-            },
-            {
-                name: "Garrett Leising",
-                linkedin: "https://www.linkedin.com/in/garrett-leising-501425159/"
-            },
-            {
-                name: "Julian Lehrer",
-                linkedin: "https://www.linkedin.com/in/julian-lehrer1/"
-            },
-            {
-                name: "Anders Poirel",
-                linkedin: "https://www.linkedin.com/in/anders-p-732595160/"
-            },
-            {
-                name: "Sean Breckenridge",
-                linkedin: "https://www.linkedin.com/in/sean-breckenridge/"
-            }
-        ]
-    },
-    {
-        projectTitle: "InSight",
-        projectDescription: "Monitors and records your mental health through emotion recongnition using Machine Learning.",
-        category: "Health Hacks",
-        path: "insight",
-        listOfWinners: [
-            {
-                name: "Neal Chokshi",
-                linkedin: "https://www.linkedin.com/in/neal-chokshi-73b00a192/"
-            },
-            {
-                name: "Vela Rajesh",
-                linkedin: "https://www.linkedin.com/in/singaravelavan-rajesh/"
-            },
-            {
-                name: "Neville Hiramanek",
-                linkedin: "https://www.linkedin.com/in/neville-hiramanek-a78754191/"
-            },
-            {
-                name: "Jaidev Gopakumar",
-                linkedin: "https://www.linkedin.com/in/jaidev-gopakumar-86a004195/"
-            }
-        ]
-    }
-]
+  {
+    projectTitle: 'TailoredChef',
+    projectDescription:
+      'An app that creates tailored-made meal recommendations for young professionals to achieve nutritional goals, develop culinary mastery, and live healthier and more sustainable lives.',
+    category: 'Health Hacks',
+    path: 'tailoredchef',
+    listOfWinners: [
+      {
+        name: 'Timothy Nguyen',
+        linkedin: 'https://www.linkedin.com/in/timothy-nguyen-cs/',
+      },
+      {
+        name: 'Austin Ha',
+        linkedin: 'https://www.linkedin.com/in/austin-ha-2885b016b/',
+      },
+      {
+        name: 'Zachary Boyd',
+        linkedin: 'https://www.linkedin.com/in/zackboydt/',
+      },
+      {
+        name: 'Leena Elzeiny',
+        linkedin: 'https://www.linkedin.com/in/leena-elzeiny-a706a4143/',
+      },
+      {
+        name: 'Noel Gomez',
+        linkedin: 'https://www.linkedin.com/in/noel-gomez/',
+      },
+    ],
+  },
+  {
+    projectTitle: 'Transparency Project',
+    projectDescription:
+      'The Transparency Project facilitates the understanding of our political process through interactive data visualization. By aggregating data from social media platforms, search engines, and ad...',
+    category: 'Justice Hacks',
+    path: 'transparencyproject',
+    listOfWinners: [
+      {
+        name: 'Oasys Okubu',
+        linkedin: 'https://www.linkedin.com/in/oasysokubo/',
+      },
+      {
+        name: 'Garrett Leising',
+        linkedin: 'https://www.linkedin.com/in/garrett-leising-501425159/',
+      },
+      {
+        name: 'Julian Lehrer',
+        linkedin: 'https://www.linkedin.com/in/julian-lehrer1/',
+      },
+      {
+        name: 'Anders Poirel',
+        linkedin: 'https://www.linkedin.com/in/anders-p-732595160/',
+      },
+      {
+        name: 'Sean Breckenridge',
+        linkedin: 'https://www.linkedin.com/in/sean-breckenridge/',
+      },
+    ],
+  },
+  {
+    projectTitle: 'InSight',
+    projectDescription:
+      'Monitors and records your mental health through emotion recongnition using Machine Learning. ',
+    category: 'Health Hacks',
+    path: 'insight',
+    listOfWinners: [
+      {
+        name: 'Neal Chokshi',
+        linkedin: 'https://www.linkedin.com/in/neal-chokshi-73b00a192/',
+      },
+      {
+        name: 'Vela Rajesh',
+        linkedin: 'https://www.linkedin.com/in/singaravelavan-rajesh/',
+      },
+      {
+        name: 'Neville Hiramanek',
+        linkedin: 'https://www.linkedin.com/in/neville-hiramanek-a78754191/',
+      },
+      {
+        name: 'Jaidev Gopakumar',
+        linkedin: 'https://www.linkedin.com/in/jaidev-gopakumar-86a004195/',
+      },
+    ],
+  },
+];
 
 const PostLandingWinnersView: React.FC = () => {
-    return(
-        <>
-            <div className="post-landing-winner__container">
-                <WinnersBoxView BoxProps={winners} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="post-landing-winner__container">
+        <WinnersBoxView BoxProps={winners} />
+      </div>
+    </>
+  );
 };
 
 export default PostLandingWinnersView;
