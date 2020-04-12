9588e4359b5b3444d7a6b0f035ca9fa1fd51ff43
mocklee <emockler@ucsc.edu>
2020-04-11 18:32:02 -0700

    hero and cta buttons responsive to smallest screen size

--- src/components/postLanding/hero/titleArea/titleArea.view.tsx
+++ src/components/postLanding/hero/titleArea/titleArea.view.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import HeroSubscribeToEmailList from '../../../landing/hero/titleArea/hero-subscribe-to-email-list.view';
+import CTAbuttonView from '../../../controls/ctaButton';
+
+const sponsorButtonProps = [
+  {
+    text: 'BRING YOUR COMPANY TO 2021',
+    link: 'mailto:sponsor@cruzhacks.com',
+    className: 'ctaButtonHero',
+  },
+];
 
 const TitleAreaView: React.FC = () => {
   return (
-    <div className="hero-title-area">
-      <div className="hero-title-area__title-text">
-        cruzhacks 2020
-        <div className="hero-title-area__slogan hero-title-area__slogan-text">
-          you.belongHere();
+    <>
+      <div className="hero-title-area">
+        <HeroSubscribeToEmailList />
+        <div className="hero-title-area__title-text">
+          cruzhacks 2020
+          <div className="hero-title-area__slogan hero-title-area__slogan-text">
+            you.belongHere();
+          </div>
         </div>
+        <CTAbuttonView info={sponsorButtonProps} />
       </div>
-      <HeroSubscribeToEmailList />
-    </div>
+    </>
   );
 };
 
